5c5197d1b9d71984f2a2d8b4391908026db0625c
flemming-n-larsen <flemming.n.larsen@gmail.com>
2016-06-20 00:28:29 +0200

    Added the JSON schema server-handshake.json and the Maven plugin jsonschema2pojo-maven-plugin to generate Java source code for it.

--- robocode2-server/src/main/java/net/robocode2/RobocodeServer.java
+++ robocode2-server/src/main/java/net/robocode2/RobocodeServer.java
@@ -1,8 +1,35 @@
 package net.robocode2;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import net.robocode2.json_schema.Game;
+import net.robocode2.json_schema.ServerHandshake;
+
 public class RobocodeServer {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Robocoders!");
+		Game game = new Game();
+		game.setArenaWidth(1000);
+		game.setArenaHeight(800);
+		game.setGameType("melee");
+		game.setMinNumberOfParticipants(2);
+		game.setMaxNumberOfParticipants(8);
+		game.setNumberOfRounds(10);
+		game.setObstacles(false);
+		game.setTimeLimit(100);
+
+		ServerHandshake handshake = new ServerHandshake();
+		Set<Game> games = new HashSet<>();
+		games.add(game);
+		handshake.setGames(games);
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(handshake);
+		
+		System.out.println(json);
 	}
 }
