26b907e76748647d117467c94daaa0aeacce89c1
flemming-n-larsen <flemming.n.larsen@gmail.com>
2016-07-29 20:54:11 +0200

    Removed ranking specific fields from the Score model class.

--- robocode2-server/src/main/java/net/robocode2/model/Score.java
+++ robocode2-server/src/main/java/net/robocode2/model/Score.java
@@ -2,34 +2,22 @@ package net.robocode2.model;
 
 public final class Score {
 
-	private final int rank;
 	private final double survival;
 	private final double lastSurvivorBonus;
 	private final double bulletDamage;
 	private final double bulletKillBonus;
 	private final double ramDamage;
 	private final double ramKillBonus;
-	private final int firstPlaces;
-	private final int secondPlaces;
-	private final int thirdPlaces;
 
-	public Score(int rank, double survival, double lastSurvivorBonus, double bulletDamage, double bulletKillBonus,
-			double ramDamage, double ramKillBonus, int firstPlaces, int secondPlaces, int thirdPlaces) {
+	public Score(double survival, double lastSurvivorBonus, double bulletDamage, double bulletKillBonus,
+			double ramDamage, double ramKillBonus) {
 
-		this.rank = rank;
 		this.survival = survival;
 		this.lastSurvivorBonus = lastSurvivorBonus;
 		this.bulletDamage = bulletDamage;
 		this.bulletKillBonus = bulletKillBonus;
 		this.ramDamage = ramDamage;
 		this.ramKillBonus = ramKillBonus;
-		this.firstPlaces = firstPlaces;
-		this.secondPlaces = secondPlaces;
-		this.thirdPlaces = thirdPlaces;
-	}
-
-	public int getRank() {
-		return rank;
 	}
 
 	public double getTotalScore() {
@@ -60,50 +48,25 @@ public final class Score {
 		return ramKillBonus;
 	}
 
-	public int getFirstPlaces() {
-		return firstPlaces;
-	}
-
-	public int getSecondPlaces() {
-		return secondPlaces;
-	}
-
-	public int getThirdPlaces() {
-		return thirdPlaces;
-	}
-
 	private Score(ScoreBuilder builder) {
-		this.rank = builder.rank;
 		this.survival = builder.survival;
 		this.lastSurvivorBonus = builder.lastSurvivorBonus;
 		this.bulletDamage = builder.bulletDamage;
 		this.bulletKillBonus = builder.bulletKillBonus;
 		this.ramDamage = builder.ramDamage;
 		this.ramKillBonus = builder.ramKillBonus;
-		this.firstPlaces = builder.firstPlaces;
-		this.secondPlaces = builder.secondPlaces;
-		this.thirdPlaces = builder.thirdPlaces;
 	}
 
 	public final static class ScoreBuilder {
-		private int rank;
 		private double survival;
 		private double lastSurvivorBonus;
 		private double bulletDamage;
 		private double bulletKillBonus;
 		private double ramDamage;
 		private double ramKillBonus;
-		private int firstPlaces;
-		private int secondPlaces;
-		private int thirdPlaces;
 
 		public Score build() {
 			return new Score(this);
-		}
-
-		public ScoreBuilder setRank(int rank) {
-			this.rank = rank;
-			return this;
 		}
 
 		public ScoreBuilder setSurvival(double survival) {
@@ -121,6 +84,11 @@ public final class Score {
 			return this;
 		}
 
+		public ScoreBuilder setBulletKillBonus(double bulletKillBonus) {
+			this.bulletKillBonus = bulletKillBonus;
+			return this;
+		}
+
 		public ScoreBuilder setRamDamage(double ramDamage) {
 			this.ramDamage = ramDamage;
 			return this;
@@ -128,21 +96,6 @@ public final class Score {
 
 		public ScoreBuilder setRamKillBonus(double ramKillBonus) {
 			this.ramKillBonus = ramKillBonus;
-			return this;
-		}
-
-		public ScoreBuilder setFirstPlaces(int firstPlaces) {
-			this.firstPlaces = firstPlaces;
-			return this;
-		}
-
-		public ScoreBuilder setSecondPlaces(int secondPlaces) {
-			this.secondPlaces = secondPlaces;
-			return this;
-		}
-
-		public ScoreBuilder setThirdPlaces(int thirdPlaces) {
-			this.thirdPlaces = thirdPlaces;
 			return this;
 		}
 	}
