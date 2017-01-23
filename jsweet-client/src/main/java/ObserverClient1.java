e5dc22c6d51fe677dedac42d67ac47bea2104fe4
flemming-n-larsen <flemming.n.larsen@gmail.com>
2017-01-23 23:41:25 +0100

    Added method for getting participant with JSweet observer.

--- jsweet-client/src/main/java/ObserverClient1.java
+++ jsweet-client/src/main/java/ObserverClient1.java
@@ -6,10 +6,12 @@ import json_schema.GameSetup;
 import json_schema.Message;
 import json_schema.NewBattleForObserver;
 import json_schema.ObserverHandshake;
+import json_schema.Participant;
 import jsweet.dom.CloseEvent;
 import jsweet.dom.Event;
 import jsweet.dom.MessageEvent;
 import jsweet.dom.WebSocket;
+import jsweet.lang.Array;
 import jsweet.lang.JSON;
 
 public class ObserverClient1 {
@@ -57,6 +59,7 @@ public class ObserverClient1 {
 		return null;
 	}
 
+	@SuppressWarnings("unchecked")
 	private Void onMessage(MessageEvent e) {
 		console.info("onMessage: " + e.toString());
 
@@ -71,7 +74,17 @@ public class ObserverClient1 {
 
 				GameSetup gameSetup = (GameSetup) $.extend(new GameSetup(), nbfo.getGameSetup());
 
+				Array<Participant> participants = (Array<Participant>) $.extend(new Array<Participant>(),
+						nbfo.getParticipants());
+
 				console.info("game type: " + gameSetup.getGameType());
+				console.info("num participants: " + participants.length);
+
+				Participant Participant1 = (Participant) $.extend(new Participant(), participants.$get(0));
+				Participant Participant2 = (Participant) $.extend(new Participant(), participants.$get(1));
+
+				console.info("name #1: " + Participant1.getName());
+				console.info("name #2: " + Participant2.getName());
 			}
 		}
 
