d89500abd7307550abd8baeb259af2ba70735363
Flemming N. Larsen <fnl@users.sourceforge.net>
2018-04-13 00:53:28 +0200

    Introduced eslint-plugin-vue in Vue code

--- vue-client/.eslintrc.js
+++ vue-client/.eslintrc.js
@@ -2,17 +2,22 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
     sourceType: 'module'
   },
   env: {
     browser: true,
   },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/essential'
+  ],
   // required to lint *.vue files
   plugins: [
+    'vue',
     'html'
   ],
   // add your custom rules here
