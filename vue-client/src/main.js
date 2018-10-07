fec2fda6599c5120c3617e25268b9fe42ebf3658
Flemming N. Larsen <fnl@users.sourceforge.net>
2018-10-07 23:25:04 +0200

    "temp.vue" => "vue-client"

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import App from './App'
-import BootstrapVue from 'bootstrap-vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import BootstrapVue from "bootstrap-vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
   router,
-  el: '#app',
-  template: '<App/>',
+  el: "#app",
+  template: "<App/>",
   components: { App }
-})
+});
