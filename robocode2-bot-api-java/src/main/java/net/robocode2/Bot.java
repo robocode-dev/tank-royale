6b7c30df99a73af3a05e004e2eafa2eef3ee052b
Flemming N. Larsen <fnl@users.sourceforge.net>
2019-05-13 01:25:43 +0200

    Implemented GameStartedEvent

--- robocode2-bot-api-java/src/main/java/net/robocode2/Bot.java
+++ robocode2-bot-api-java/src/main/java/net/robocode2/Bot.java
@@ -4,19 +4,17 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import lombok.NonNull;
 import lombok.val;
 import lombok.var;
 import net.robocode2.events.ConnectedEvent;
 import net.robocode2.events.ConnectionErrorEvent;
 import net.robocode2.events.DisconnectedEvent;
-import net.robocode2.schema.BotHandshake;
-import net.robocode2.schema.BotInfo;
-import net.robocode2.schema.ServerHandshake;
+import net.robocode2.schema.*;
 import org.java_websocket.client.WebSocketClient;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 
 public abstract class Bot implements IBot {
 
@@ -24,16 +22,53 @@ public abstract class Bot implements IBot {
 
   // Default constructor is not allowed, and thus must be hidden
   private Bot() {
-    __internals = null;
+    __internals = new __Internals(null);
   }
 
-  public Bot(final BotInfo botInfo) {
+  public Bot(@NonNull final BotInfo botInfo) {
     __internals = new __Internals(botInfo);
   }
 
-  public Bot(final BotInfo botInfo, URI serverUri) {
+  public Bot(@NonNull final BotInfo botInfo, @NonNull URI serverUri) {
     __internals = new __Internals(botInfo, serverUri);
   }
+
+  public int getMyId() {
+    return __internals.myId;
+  }
+
+  public String getGameType() {
+    return __internals.gameType;
+  }
+
+  public int getArenaWidth() {
+    return __internals.arenaWidth;
+  }
+
+  public int getArenaHeight() {
+    return __internals.arenaHeight;
+  }
+
+  public int getNumberOfRounds() {
+    return __internals.numberOfRounds;
+  }
+
+  public double getGunCoolingRate() {
+    return __internals.gunCoolingRate;
+  }
+
+  public int getInactivityTurns() {
+    return __internals.inactivityTurns;
+  }
+
+  public int getTurnTimeout() {
+    return __internals.turnTimeout;
+  }
+
+  public int getReadyTimeout() {
+    return __internals.readyTimeout;
+  }
+
 
   private final class __Internals {
     private static final String SERVER_URI_PROPERTY_KEY = "server.uri";
@@ -47,9 +82,31 @@ public abstract class Bot implements IBot {
 
     private String clientKey;
 
-    public __Internals(BotInfo botInfo) {
-      this.botInfo = botInfo;
+    private int myId;
+    private String gameType;
+    private int arenaWidth;
+    private int arenaHeight;
+    private int numberOfRounds;
+    private double gunCoolingRate;
+    private int inactivityTurns;
+    private int turnTimeout;
+    private int readyTimeout;
 
+    __Internals(BotInfo botInfo) {
+      this.botInfo = botInfo;
+      init(getServerUriSetting());
+    }
+
+    __Internals(BotInfo botInfo, URI serverUri) {
+      this.botInfo = botInfo;
+      init(serverUri);
+    }
+
+    private void init(URI serverUri) {
+      wsClient = new WSClient(serverUri);
+    }
+
+    private URI getServerUriSetting() {
       var uri = System.getProperty(SERVER_URI_PROPERTY_KEY);
       if (uri == null) {
         uri = System.getenv(SERVER_URI_ENV_VAR);
@@ -61,19 +118,10 @@ public abstract class Bot implements IBot {
                 SERVER_URI_PROPERTY_KEY, SERVER_URI_ENV_VAR));
       }
       try {
-        init(new URI(uri));
+        return new URI(uri);
       } catch (URISyntaxException ex) {
         throw new BotException("Incorrect syntax for server uri: " + uri);
       }
-    }
-
-    public __Internals(BotInfo botInfo, URI serverUri) {
-      this.botInfo = botInfo;
-      init(serverUri);
-    }
-
-    private void init(URI serverUri) {
-      wsClient = new WSClient(serverUri);
     }
 
     private final class WSClient extends WebSocketClient {
@@ -106,37 +154,77 @@ public abstract class Bot implements IBot {
 
       @Override
       public final void onMessage(final String message) {
-        JsonObject jsonMessage = gson.fromJson(message, JsonObject.class);
+        JsonObject jsonMsg = gson.fromJson(message, JsonObject.class);
 
-        JsonElement jsonType = jsonMessage.get("type");
+        JsonElement jsonType = jsonMsg.get("type");
         if (jsonType != null) {
-          String type = jsonType.getAsString();
+          val type = jsonType.getAsString();
 
-          if (ServerHandshake.Type.SERVER_HANDSHAKE.toString().equalsIgnoreCase(type)) {
-            // The client key is assigned to the bot from the server only
-            clientKey = jsonMessage.get("clientKey").getAsString();
-
-            // Send bot handshake
-            BotHandshake handshake = new BotHandshake();
-            handshake.setType(BotHandshake.Type.BOT_HANDSHAKE);
-            handshake.setClientKey(clientKey);
-            handshake.setName("Bot name");
-            handshake.setVersion("0.1");
-            handshake.setAuthor("Author name");
-            handshake.setCountryCode("DK");
-            handshake.setGameTypes(Arrays.asList("melee", "1v1"));
-            handshake.setProgrammingLang("Java");
-
-            String msg = gson.toJson(handshake);
-            send(msg);
+          switch (Event.Type.fromValue(type)) {
+            case GAME_STARTED_EVENT_FOR_BOT:
+              handleGameStartedEvent(jsonMsg);
+              break;
+            default:
+              switch (Message.Type.fromValue(type)) {
+                case SERVER_HANDSHAKE:
+                  handleServerHandshake(jsonMsg);
+                  break;
+              }
           }
         }
         // TODO
       }
+
+      private void handleServerHandshake(JsonObject jsonMsg) {
+        // The client key is assigned to the bot from the server only
+        clientKey = jsonMsg.get("clientKey").getAsString();
+
+        // Send bot handshake
+        val msg = gson.toJson(createBotHandshake());
+        send(msg);
+      }
+
+      private void handleGameStartedEvent(JsonObject jsonMsg) {
+        val gameStartedEvent = gson.fromJson(jsonMsg, GameStartedEventForBot.class);
+        val gameSetup = gameStartedEvent.getGameSetup();
+
+        myId = gameStartedEvent.getMyId();
+        gameType = gameSetup.getGameType();
+        arenaWidth = gameSetup.getArenaWidth();
+        arenaHeight = gameSetup.getArenaHeight();
+        numberOfRounds = gameSetup.getNumberOfRounds();
+        gunCoolingRate = gameSetup.getGunCoolingRate();
+        inactivityTurns = gameSetup.getInactivityTurns();
+        turnTimeout = gameSetup.getTurnTimeout();
+        readyTimeout = gameSetup.getReadyTimeout();
+
+        // Send ready signal
+        BotReady ready = new BotReady();
+        ready.setType(BotReady.Type.BOT_READY);
+        ready.setClientKey(clientKey);
+
+        val msg = gson.toJson(ready);
+        send(msg);
+
+        Bot.this.onGameStarted();
+      }
+
+      private BotHandshake createBotHandshake() {
+        BotHandshake handshake = new BotHandshake();
+        handshake.setType(BotHandshake.Type.BOT_HANDSHAKE);
+        handshake.setClientKey(clientKey);
+        handshake.setName(botInfo.getName());
+        handshake.setVersion(botInfo.getVersion());
+        handshake.setAuthor(botInfo.getAuthor());
+        handshake.setCountryCode(botInfo.getCountryCode());
+        handshake.setGameTypes(botInfo.getGameTypes());
+        handshake.setProgrammingLang(botInfo.getProgrammingLang());
+        return handshake;
+      }
     }
   }
 
-  public final class BotException extends RuntimeException {
+  public static final class BotException extends RuntimeException {
     BotException(final String message) {
       super(message);
     }
--- robocode2-bot-api-java/src/main/java/net/robocode2/IBot.java
+++ robocode2-bot-api-java/src/main/java/net/robocode2/IBot.java
@@ -7,6 +7,45 @@ import net.robocode2.events.DisconnectedEvent;
 /** Interface for a bot. */
 public interface IBot {
 
+  /** Returns the unique id of this bot in the battle. Available when game has started. */
+  int getMyId();
+
+  /** Returns the game type, e.g. "melee". Available when game has started. */
+  String getGameType();
+
+  /** Returns the width of the arena measured in pixels. Available when game has started. */
+  int getArenaWidth();
+
+  /** Returns the height of the arena measured in pixels. Available when game has started. */
+  int getArenaHeight();
+
+  /** Returns the number of rounds in a battle. Available when game has started. */
+  int getNumberOfRounds();
+
+  /**
+   * Returns the gun cooling rate. The gun needs to cool down to a gun heat of zero before the gun
+   * is able to fire. Available when game has started.
+   */
+  double getGunCoolingRate();
+
+  /**
+   * Returns the maximum number of inactive turns allowed, where a bot does not take any action
+   * before it is zapped by the game. Available when game has started.
+   */
+  int getInactivityTurns();
+
+  /**
+   * Returns turn timeout in milliseconds for sending intent after having received 'tick' message.
+   * Available when game has started.
+   */
+  int getTurnTimeout();
+
+  /**
+   * Returns time limit in milliseconds for sending ready message after having received 'new battle'
+   * message. Available when game has started.
+   */
+  int getReadyTimeout();
+
   /** Event handler triggered when connected to server */
   void onConnected(ConnectedEvent connectedEvent);
 
@@ -15,4 +54,7 @@ public interface IBot {
 
   /** Event handler triggered when a connection error occurs */
   void onConnectionError(ConnectionErrorEvent connectionErrorEvent);
+
+  /** Event handler triggered when game has started */
+  void onGameStarted();
 }
