eb7646478172b69fbe9e89868ca2ce883422a65f
Flemming N. Larsen <fnl@users.sourceforge.net>
2019-09-14 00:20:05 +0200

    Fixed bug with misc settings, which where never loaded

--- robocode-tankroyale-ui-desktop/src/main/kotlin/dev/robocode/tankroyale/ui/desktop/settings/MiscSettings.kt
+++ robocode-tankroyale-ui-desktop/src/main/kotlin/dev/robocode/tankroyale/ui/desktop/settings/MiscSettings.kt
@@ -1,7 +1,5 @@
 package dev.robocode.tankroyale.ui.desktop.settings
 
-import java.nio.file.Path
-
 object MiscSettings : PropertiesStore("Robocode Misc Config", "misc.properties") {
 
     private const val BOT_DIRECTORIES_PROPERTY = "bot.directories"
@@ -9,10 +7,14 @@ object MiscSettings : PropertiesStore("Robocode Misc Config", "misc.properties")
 
     const val BOT_DIRS_SEPARATOR = ";"
 
+    init {
+        load()
+    }
+
     var botsDirectories: List<String>
         get() =
-            properties.getProperty(BOT_DIRECTORIES_PROPERTY, DEFAULT_BOT_DIRECTORIES).split(";")
+            properties.getProperty(BOT_DIRECTORIES_PROPERTY, DEFAULT_BOT_DIRECTORIES).split(BOT_DIRS_SEPARATOR)
         set(value) {
-            properties.setProperty(BOT_DIRECTORIES_PROPERTY, value.joinToString(separator = ";"))
+            properties.setProperty(BOT_DIRECTORIES_PROPERTY, value.joinToString(separator = BOT_DIRS_SEPARATOR))
         }
 }
