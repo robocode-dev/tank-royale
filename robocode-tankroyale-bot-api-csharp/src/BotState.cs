590e4076b449d58827bc938c40e0292587e82b88
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2020-03-15 22:22:07 +0100

    Updated documentation for BotInfo.

--- robocode-tankroyale-bot-api-csharp/src/BotState.cs
+++ robocode-tankroyale-bot-api-csharp/src/BotState.cs
@@ -7,34 +7,43 @@ namespace Robocode.TankRoyale.BotApi
   public sealed class BotState
   {
     /// <summary>Energy level.</summary>
+    /// <value>The energy level.</value>
     public double Energy { get; }
 
     /// <summary>X coordinate.</summary>
+    /// <value>The X coordinate.</value>
     public double X { get; }
 
     /// <summary>Y coordinate.</summary>
+    /// <value>The Y coordinate.</value>
     public double Y { get; }
 
     /// <summary>Driving direction in degrees.</summary>
+    /// <value>The driving direction in degrees.</value>
     public double Direction { get; }
 
     /// <summary>Gun direction in degrees.</summary>
+    /// <value>The gun direction in degrees.</value>
     public double GunDirection { get; }
 
     /// <summary>Radar direction in degrees.</summary>
+    /// <value>The radar direction in degrees.</value>
     public double RadarDirection { get; }
 
-    /// <summary>Radar sweep angle in degrees, i.e. angle between previous and current radar direction.</summary>
+    /// <summary>Radar sweep angle in degrees.</summary>
+    /// <value>The radar sweep angle in degrees.</value>
     public double RadarSweep { get; }
 
     /// <summary>Speed measured in pixels per turn.</summary>
+    /// <value>The speed measured in pixels per turn.</value>
     public double Speed { get; }
 
     /// <summary>Gun heat.</summary>
+    /// <value>The gun heat.</value>
     public double GunHeat { get; }
 
     /// <summary>
-    /// Constructor.
+    /// Initializes a new instance of the BotState class.
     /// </summary>
     /// <param name="energy">Energy level.</param>
     /// <param name="x">X coordinate.</param>
@@ -42,7 +51,7 @@ namespace Robocode.TankRoyale.BotApi
     /// <param name="direction">Driving direction in degrees.</param>
     /// <param name="gunDirection">Gun direction in degrees.</param>
     /// <param name="radarDirection">Radar direction in degrees.</param>
-    /// <param name="radarSweep">Radar sweep angle in degrees, i.e. angle between previous and current radar direction.</param>
+    /// <param name="radarSweep">Radar sweep angle in degrees.</param>
     /// <param name="speed">Speed measured in pixels per turn.</param>
     /// <param name="gunHeat">Gun heat.</param>
     [JsonConstructor]
--- robocode-tankroyale-bot-api-java/src/main/java/dev/robocode/tankroyale/botapi/BotState.java
+++ robocode-tankroyale-bot-api-java/src/main/java/dev/robocode/tankroyale/botapi/BotState.java
@@ -22,7 +22,7 @@ public final class BotState {
   /** Radar direction in degrees */
   private final double radarDirection;
 
-  /** Radar sweep angle in degrees, i.e. angle between previous and current radar direction */
+  /** Radar sweep angle in degrees */
   private final double radarSweep;
 
   /** Speed measured in pixels per turn */
@@ -31,6 +31,19 @@ public final class BotState {
   /** Gun heat */
   private final double gunHeat;
 
+  /**
+   * Initializes a new instance of the BotState class.
+   *
+   * @param energy is the energy level.
+   * @param x is the X coordinate.
+   * @param y is the Y coordinate.
+   * @param direction is the driving direction in degrees.
+   * @param gunDirection is the gun direction in degrees.
+   * @param radarDirection is the radar direction in degrees.
+   * @param radarSweep is the radar sweep angle in degrees.
+   * @param speed is the speed measured in pixels per turn.
+   * @param gunHeat is the gun heat.
+   */
   public BotState(
       double energy,
       double x,
@@ -52,50 +65,90 @@ public final class BotState {
     this.gunHeat = gunHeat;
   }
 
-  /** Returns the energy level */
+  /**
+   * Returns the energy level of the bot. The energy level is typically starting at 100. The bot
+   * gains more energy when hitting other bots, and loses energy by getting hit and when spending
+   * energy on firing bullets. When the energy reaches 0, the bot becomes disabled and will not be
+   * able to take any new action. It might become active again, if one of its bullets hit another
+   * bot, meaning that the bot gains new energy.
+   *
+   * @return the energy level.
+   */
   public double getEnergy() {
     return energy;
   }
 
-  /** Returns the X coordinate */
+  /**
+   * Returns the X coordinate of the bot, which is in the center of the bot.
+   *
+   * @return the X coordinate.
+   */
   public double getX() {
     return x;
   }
 
-  /** Returns the Y coordinate */
+  /**
+   * Returns the Y coordinate of the bot, which is in the center of the bot.
+   *
+   * @return the Y coordinate.
+   */
   public double getY() {
     return y;
   }
 
-  /** Returns the driving direction in degrees */
+  /**
+   * Returns the driving direction of the bot in degrees.
+   *
+   * @return the driving direction.
+   */
   public double getDirection() {
     return direction;
   }
 
-  /** Returns the gun direction in degrees */
+  /**
+   * Returns the gun direction of the bot in degrees.
+   *
+   * @return the gun direction.
+   */
   public double getGunDirection() {
     return gunDirection;
   }
 
-  /** Returns the radar direction in degrees */
+  /**
+   * Returns the radar direction of the bot in degrees.
+   *
+   * @return the radar direction.
+   */
   public double getRadarDirection() {
     return radarDirection;
   }
 
   /**
-   * Returns the radar sweep angle in degrees, i.e. angle between previous and current radar
-   * direction
+   * Returns the radar sweep angle in degrees, i.e. delta angle between previous and current radar
+   * direction.
+   *
+   * @return the radar sweep angle.
    */
   public double getRadarSweep() {
     return radarSweep;
   }
 
-  /** Returns the speed measured in pixels per turn */
+  /**
+   * Returns the speed measured in units per turn.
+   *
+   * @return the speed.
+   */
   public double getSpeed() {
     return speed;
   }
 
-  /** Returns the gun heat */
+  /**
+   * Returns the gun heat. When firing the gun, it will be heated up. The gun will need to cool down
+   * before it can fire another bullet. When the gun heat is zero, the gun will be able to fire
+   * again.
+   *
+   * @return the gun heat.
+   */
   public double getGunHeat() {
     return gunHeat;
   }
