70df6283d88a056072af8e7864b32f27b51b93f1
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2020-11-16 23:56:28 +0100

    Upgraded for Kotlin 1.4.10, Kotlinx serialization 1.0.0, and Picocli 4.5.2

--- robocode-tankroyale-bootstrap/src/main/kotlin/dev/robocode/tankroyale/bootstrap/commands/Command.kt
+++ robocode-tankroyale-bootstrap/src/main/kotlin/dev/robocode/tankroyale/bootstrap/commands/Command.kt
@@ -1,34 +1,21 @@
 package dev.robocode.tankroyale.bootstrap.commands
 
-import dev.robocode.tankroyale.bootstrap.BootstrapException
 import dev.robocode.tankroyale.bootstrap.model.BotInfo
-import kotlinx.serialization.ImplicitReflectionSerializer
-import kotlinx.serialization.MissingFieldException
-import kotlinx.serialization.UnstableDefault
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonDecodingException
-import kotlinx.serialization.parse
 import java.nio.file.Files
 import java.nio.file.Path
 
-@UnstableDefault
-@ImplicitReflectionSerializer
 abstract class Command(private val botPaths: List<Path>) {
 
     protected fun getBotInfo(botName: String): BotInfo? {
-        var path: Path? = null
+        var path: Path?
         botPaths.forEach { dirPath ->
             run {
-                try {
-                    path = resolveFullBotPath(dirPath, "$botName.json")?.toAbsolutePath()
-                    if (path != null) {
-                        val content = path!!.toFile().readText(Charsets.UTF_8)
-                        return Json.parse(content)
-                    }
-                } catch (ex: JsonDecodingException) {
-                    throw BootstrapException("Could not parse JSON file: $path")
-                } catch (ex: MissingFieldException) {
-                    throw BootstrapException("${ex.message}. File: $path")
+                path = resolveFullBotPath(dirPath, "$botName.json")?.toAbsolutePath()
+                if (path != null) {
+                    val content = path!!.toFile().readText(Charsets.UTF_8)
+                    return Json.decodeFromString(content)
                 }
             }
         }
