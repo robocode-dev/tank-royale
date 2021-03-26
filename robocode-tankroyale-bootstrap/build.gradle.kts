535a63f79ddfcb89a35e9dcd8e2d0ce239564ff9
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2021-03-27 00:22:34 +0100

    Replaced Groovy based gradle.build with Kotlin based gradle.build.kts

---
+++ robocode-tankroyale-bootstrap/build.gradle.kts
@@ -0,0 +1,85 @@
+import org.apache.tools.ant.filters.ReplaceTokens
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val title = "Robocode Royale Bootstrap"
+description = "Bootstrap utility for booting up bots for Robocode Tank Royale"
+
+group = "dev.robocode.tankroyale"
+val artifactId = "robocode-tankroyale-bootstrap"
+version = "0.7.0"
+
+
+plugins {
+    `java-library`
+    `maven-publish`
+    kotlin("jvm") version "1.5.0-M1"
+    kotlin("plugin.serialization") version "1.5.0-M1"
+    idea
+
+    id("com.github.ben-manes.versions") version "0.28.0"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_9
+}
+
+idea {
+    module {
+        outputDir = file("$buildDir/classes/kotlin/main")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.0-M1")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.0-M1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.1.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
+
+    implementation("info.picocli:picocli:4.5.2")
+}
+
+tasks.processResources {
+    with(copySpec {
+        from("src/main/resources")
+        include("version.txt")
+        filter(ReplaceTokens::class, "tokens" to mapOf("version" to version))
+    })
+}
+
+val fatJar = task<Jar>("fatJar") {
+    manifest {
+        attributes["Implementation-Title"] = title
+        attributes["Implementation-Version"] = archiveVersion
+        attributes["Main-Class"] = "dev.robocode.tankroyale.bootstrap.BootstrapKt"
+    }
+    from(
+        configurations.compile.get().filter { it.name.endsWith("jar") }.map { zipTree(it) },
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    )
+    with(tasks["jar"] as CopySpec)
+}
+
+tasks.named("build") {
+    dependsOn(fatJar)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group as String?
+            artifactId
+            version
+        }
+    }
+}
