10dc085389cc687cc08ab47db9b107dd5300eeb9
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2021-05-10 22:56:53 +0200

    Updated nuget_push.bat

--- robocode-tankroyale-bot-api-dotnet/nuget_push.bat
+++ robocode-tankroyale-bot-api-dotnet/nuget_push.bat
@@ -1,5 +1,5 @@
 dotnet clean
-dotnet build
-dotnet pack
-cd bin\Debug
-dotnet nuget push .\Robocode.TankRoyale.BotApi.0.7.1.nupkg -s D:\LocalNugetPackages
+dotnet build --configuration Release
+dotnet pack --configuration Release
+cd .\bin\Release\
+dotnet nuget push .\Robocode.TankRoyale.BotApi.0.9.1.nupkg -s D:\LocalNugetPackages
