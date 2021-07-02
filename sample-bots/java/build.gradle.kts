c9849f2928033ecd879c6b63abed35da33aee218
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2021-07-03 00:23:29 +0200

    Work on build script for creating archive of Java sample bots.

--- sample-bots/java/build.gradle.kts
+++ sample-bots/java/build.gradle.kts
@@ -1,39 +1,63 @@
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.Files
+import java.io.File
+
+import java.util.Comparator
+
+
 abstract class JavaSampleBotsTask : DefaultTask() {
+
     @TaskAction
     fun task() {
-        val files = File(".").list()
+        val currentWorkingDir = Paths.get(System.getProperty("user.dir"))
 
-        files.forEach { filename -> run {
-            val file = File(filename)
-            if (file.isDirectory && !filename.startsWith(".") && !filename.equals("gradle")) {
-                val jarFilename = getJarArchiveName(file)
-                if (jarFilename != null) {
-                    println(jarFilename)
-                }
-            }
+        val archiveDir = currentWorkingDir.resolve(".archive")
+        deleteDir(archiveDir)
+        createDir(archiveDir)
+
+        val libsDir = archiveDir.resolve("libs")
+        createDir(libsDir)
+
+        val currentWorkDir = Paths.get(System.getProperty("user.dir"))
+
+        Files.list(currentWorkDir).forEach { projectDir -> run {
+            copyBotJarArchive(projectDir, libsDir)
         }}
     }
 
-    private fun runMavenBuild(dir: File) {
-        println("Running Maven build for: $dir")
-
-        var mvn = System.getenv("MAVEN_HOME") + "/bin/mvn"
-        if (System.getProperty("os.name").startsWith("Windows")) {
-           mvn += ".cmd"
+    private fun createDir(path: Path) {
+        if (!Files.exists(path)) {
+            Files.createDirectory(path)
         }
-        val pb = ProcessBuilder(mvn, "clean", "package")
-        pb.directory(File(System.getProperty("user.dir"), dir.name))
-        pb.start()
     }
 
-    private fun getJarArchiveName(dir: File): String? {
-        val dir2 = File(System.getProperty("user.dir"), dir.name + "/target")
-        dir2.list()?.forEach { filename ->
-            if (filename.startsWith(dir.name)) {
-                return filename
+    private fun deleteDir(path: Path) {
+        if (Files.exists(path)) {
+            Files.walk(path)
+                .sorted(Comparator.reverseOrder())
+                .map(({ obj: Path -> obj.toFile() }))
+                .forEach(({ obj: File -> obj.delete() }))
+        }
+    }
+
+    private fun copyBotJarArchive(projectDir: Path, destDir: Path) {
+        if (Files.isDirectory(projectDir) && !projectDir.fileName.toString().startsWith(".")) {
+            val jarFilename = getJarBotArchiveName(projectDir)
+            if (jarFilename != null) {
+                Files.copy(jarFilename, destDir.resolve(jarFilename.fileName))
             }
         }
-        System.err.println("Could not find jar archive in dir: $dir")
+    }
+
+    private fun getJarBotArchiveName(projectDir: Path): Path? {
+        val buildDir: Path = Paths.get(System.getProperty("user.dir")).resolve(projectDir).resolve("build/libs")
+        for (dir in Files.list(buildDir)) {
+            if (dir.startsWith(projectDir)) {
+                return buildDir.resolve(dir)
+            }
+        }
+        System.err.println("Could not find jar archive in dir: $projectDir")
         return null
     }
 }
