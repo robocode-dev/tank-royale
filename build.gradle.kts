183d2600f792aaf2aeac0cb9e995db53f25d2e96
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2021-10-15 19:28:35 +0200

    Added Hierynomus License formatter to insert a Apache License header on source files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,35 @@
+import java.time.Year
+import com.hierynomus.gradle.license.tasks.LicenseFormat
+
 plugins {
     kotlin("jvm") version "1.5.30"
     kotlin("plugin.serialization") version "1.5.30"
     id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.github.hierynomus.license-base") version "0.16.1"
+}
+
+subprojects {
+    apply(plugin = "com.github.hierynomus.license-base")
+
+    license {
+        header = rootProject.file("LICENSE.header")
+        encoding = "UTF-8"
+//        skipExistingHeaders = true
+        mapping("java", "SLASHSTAR_STYLE")
+        mapping("kt", "SLASHSTAR_STYLE")
+        mapping("svg", "XML_STYLE")
+        exclude("**/META-INF/**")
+        exclude("**/*.png")
+        exclude("**/*.txt")
+
+        var ext = this as ExtensionAware
+        with (ext) {
+            extra["year"] = Year.now()
+            extra["name"] = "Flemming Nørnberg Larsen"
+        }
+    }
+}
+
+tasks.named("assemble") {
+    dependsOn("licenseFormatMain")
 }
