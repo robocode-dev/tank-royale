075b0dccc006937149f8042a08d5440bbc290390
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2021-11-05 22:42:38 +0100

    Created build.gradle.kts to use gradle plugin for jsonschema2pojo instead of Maven version.

---
+++ schema/java/build.gradle.kts
@@ -0,0 +1,66 @@
+import org.jsonschema2pojo.AnnotationStyle
+import org.jsonschema2pojo.SourceType
+import java.util.Collections.singletonList
+import org.jsonschema2pojo.gradle.JsonSchemaExtension
+import java.lang.reflect.Field
+
+val title = "Robocode Tank Royale Schema"
+description = "Schema for Robocode Tank Royale"
+
+group = "dev.robocode.tankroyale"
+version = "0.8.1"
+
+val artifactBaseName = "robocode-tankroyale-schema"
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jsonschema2pojo:jsonschema2pojo-gradle-plugin:1.1.1")
+    }
+}
+
+plugins {
+    `java-library`
+    idea
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.8.7")
+}
+
+apply(plugin = "java")
+apply(plugin = "jsonschema2pojo")
+
+// https://github.com/joelittlejohn/jsonschema2pojo/blob/master/jsonschema2pojo-gradle-plugin/src/main/groovy/org/jsonschema2pojo/gradle/JsonSchemaExtension.groovy
+configure<JsonSchemaExtension> {
+    setSourceType(SourceType.YAMLSCHEMA.toString())
+    setSource(singletonList(File("$projectDir/../schemas")))
+    setAnnotationStyle(AnnotationStyle.GSON.toString())
+    targetPackage = "dev.robocode.tankroyale.schema"
+}
+
+val fatJar = task<Jar>("fatJar") {
+    archiveBaseName.set(artifactBaseName)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Implementation-Title"] = title
+        attributes["Implementation-Version"] = archiveVersion
+    }
+    from(
+        configurations.compileClasspath.get().filter { it.name.endsWith(".jar") }.map { zipTree(it) },
+        configurations.runtimeClasspath.get().filter { it.name.endsWith(".jar") }.map { zipTree(it) }
+    )
+    with(tasks["jar"] as CopySpec)
+}
+
+tasks.named("build") {
+    dependsOn(fatJar)
+}
