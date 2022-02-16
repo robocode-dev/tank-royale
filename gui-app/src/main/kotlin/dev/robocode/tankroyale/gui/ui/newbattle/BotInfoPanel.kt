886089707f5df7005db5cba397f5b67966dec6fe
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-02-16 20:00:04 +0100

    Added column to the center of 10 pixels.

--- gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/ui/newbattle/BotInfoPanel.kt
+++ gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/ui/newbattle/BotInfoPanel.kt
@@ -7,7 +7,7 @@ import java.awt.Dimension
 import java.util.*
 import javax.swing.*
 
-object BotInfoPanel : JPanel(MigLayout("fillx", "[min][sg fields,grow][min][sg fields,grow]")) {
+object BotInfoPanel : JPanel(MigLayout("", "[][sg a,grow][10][][sg a,grow]")) {
 
     private val nameTextField = JNonEditableTextField()
     private val versionTextField = JNonEditableTextField()
@@ -22,37 +22,37 @@ object BotInfoPanel : JPanel(MigLayout("fillx", "[min][sg fields,grow][min][sg f
     init {
         border = BorderFactory.createTitledBorder("Bot Info")
 
-        addLabel("bot_info.name")
-        add(nameTextField, "growx")
+        addLabel("bot_info.name", "cell 0 0")
+        add(nameTextField, "cell 1 0, growx")
 
-        addLabel("bot_info.platform")
-        add(platformTextField, "growx, wrap")
+        addLabel("bot_info.version", "cell 0 1")
+        add(versionTextField, "cell 1 1, growx")
 
-        addLabel("bot_info.version")
-        add(versionTextField, "growx")
+        addLabel("bot_info.authors", "cell 0 2")
+        add(authorsTextField, "cell 1 2, growx")
 
-        addLabel("bot_info.programming_lang")
-        add(programmingLangTextField, "growx, wrap")
-
-        addLabel("bot_info.authors")
-        add(authorsTextField, "growx")
-
-        addLabel("bot_info.game_types")
-        add(gameTypesTextField, "growx, wrap")
-
-        addLabel("bot_info.homepage")
-        add(homepageTextPane, "growx")
+        addLabel("bot_info.homepage", "cell 0 3")
+        add(homepageTextPane, "cell 1 3, growx")
         homepageTextPane.minimumSize = nameTextField.minimumSize
 
-        addLabel("bot_info.country_codes")
-        add(countryCodesTextPane, "growx, wrap")
-        countryCodesTextPane.minimumSize = Dimension(100, 24)
-
-        addLabel("bot_info.description")
-        add(descriptionTextField, "growx, span 3")
+        addLabel("bot_info.description", "cell 0 4")
+        add(descriptionTextField, "growx, span 4")
         descriptionTextField.border = nameTextField.border
         descriptionTextField.background = background
         descriptionTextField.font = font
+
+        addLabel("bot_info.platform", "cell 3 0")
+        add(platformTextField, "cell 4 0, growx")
+
+        addLabel("bot_info.programming_lang", "cell 3 1")
+        add(programmingLangTextField, "cell 4 1, growx")
+
+        addLabel("bot_info.game_types", "cell 3 2")
+        add(gameTypesTextField, "cell 4 2, growx")
+
+        addLabel("bot_info.country_codes", "cell 3 3")
+        add(countryCodesTextPane, "cell 4 3, growx")
+        countryCodesTextPane.minimumSize = Dimension(100, 24)
 
         updateBotInfo(null)
 
