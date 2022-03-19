8e647755f477b3332c0f0f0c9e9a12512e633b01
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-03-19 22:11:58 +0100

    Added internal InitialPosition class with unit test.

---
+++ bot-api/dotnet/Robocode.TankRoyale.BotApi.Tests/src/internal/InitialPositionTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using Robocode.TankRoyale.BotApi.Internal;
+
+namespace Robocode.TankRoyale.BotApi.Tests.Internal
+{
+    public class InitialPositionTests
+    {
+        private const double Tolerance = 0.000000000000005;
+
+        [Test]
+        [TestCase("50, 50, 270", 50.0, 50.0, 270.0)]
+        [TestCase("12.23, -123.3, 45.5", 12.23, -123.3, 45.5)]
+        [TestCase(" 50 ", 50.0, null, null)]
+        [TestCase(" 50.1  70.2 ", 50.1, 70.2, null)]
+        [TestCase("50.1 70.2, 678.3", 50.1, 70.2, 678.3)]
+        [TestCase("50.1  , 70.2, 678.3", 50.1, 70.2, 678.3)]
+        [TestCase("50.1 70.2, 678.3 789.1", 50.1, 70.2, 678.3)]
+        [TestCase("50.1  , , 678.3", 50.1, null, 678.3)]
+        [TestCase(", , 678.3", null, null, 678.3)]
+        public void FromString_ShouldWork(string str, double? x, double? y, double? angle)
+        {
+            var pos = InitialPosition.FromString(str);
+            Assert.That(pos.X, Is.EqualTo(x).Within(Tolerance));
+            Assert.That(pos.Y, Is.EqualTo(y).Within(Tolerance));
+            Assert.That(pos.Angle, Is.EqualTo(angle).Within(Tolerance));
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(" \t")]
+        [TestCase("  ")]
+        [TestCase(",,,")]
+        [TestCase(", ,")]
+        public void FromString_ShouldReturnNull(string str)
+        {
+            var pos = InitialPosition.FromString(str);
+            Assert.That(pos, Is.Null);
+        }
+        
+        [Test]
+        [TestCase("50, 50, 270", "50,50,270")]
+        [TestCase("12.23, -123.3, 45.5", "12.23,-123.3,45.5")]
+        [TestCase(" 50 ", "50,,")]
+        [TestCase(" 50.1  70.2 ", "50.1,70.2,")]
+        [TestCase("50.1 70.2, 678.3", "50.1,70.2,678.3")]
+        [TestCase("50.1  , 70.2, 678.3", "50.1,70.2,678.3")]
+        [TestCase("50.1 70.2, 678.3 789.1", "50.1,70.2,678.3")]
+        [TestCase("50.1  , , 678.3", "50.1,,678.3")]
+        [TestCase(", , 678.3", ",,678.3")]
+        [TestCase("", "")]
+        [TestCase(" \t", "")]
+        [TestCase(" ,", "")]
+        [TestCase(",,,", "")]
+        [TestCase(", ,", "")]
+        public void ToString_ShouldWork(string input, string expected) {
+            var pos = InitialPosition.FromString(input);
+            if (pos == null)
+            {
+                Assert.That(expected, Is.Empty);
+            } else {
+                Assert.That(pos.ToString(), Is.EqualTo(expected));
+            }
+        }
+    }
+}
---
+++ bot-api/dotnet/Robocode.TankRoyale.BotApi/src/internal/InitialPosition.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Robocode.TankRoyale.BotApi.Internal
+{
+    /// <summary>
+    /// Initial starting position containing a start coordinate (x,y) and angle.
+    ///
+    /// The initial position is only used when debugging to request the server to let a bot start at a specific position.
+    /// Note that initial starting positions must be enabled at the server-side; otherwise the initial starting position
+    /// is ignored.
+    /// </summary>
+    public sealed class InitialPosition
+    {
+        /// <summary>
+        /// Initializes a new instance of the InitialPosition class.
+        /// </summary>
+        /// <param name="x">The x coordinate, where <c>null</c> means it is random.</param>
+        /// <param name="y">The x coordinate, where <c>null</c> means it is random.</param>
+        /// <param name="angle">The angle, where <c>null</c> means it is random.</param>
+        internal InitialPosition(double? x, double? y, double? angle)
+        {
+            X = x;
+            Y = y;
+            Angle = angle;
+        }
+
+        /// <summary>
+        /// The x coordinate.
+        /// </summary>
+        public double? X { get; }
+
+        /// <summary>
+        /// The Y coordinate.
+        /// </summary>
+        public double? Y { get; }
+
+        /// <summary>
+        /// The Angle coordinate.
+        /// </summary>
+        public double? Angle { get; }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "{0},{1},{2}", X, Y, Angle);
+        }
+
+        public static InitialPosition FromString(string initialPosition)
+        {
+            if (initialPosition == null || string.IsNullOrWhiteSpace(initialPosition)) return null;
+            var values = Regex.Split(initialPosition.Trim(), @"\s*,\s*|\s+");
+            return ParseInitialPosition(values);
+        }
+
+        private static InitialPosition ParseInitialPosition(string[] values)
+        {
+            if (values.Length < 1) return null;
+
+            var x = ParseDouble(values[0]);
+            if (values.Length < 2)
+            {
+                return new InitialPosition(x, null, null);
+            }
+            var y = ParseDouble(values[1]);
+            double? angle = null;
+            if (values.Length >= 3)
+            {
+                angle = ParseDouble(values[2]);
+            }
+
+            if (x == null && y == null && angle == null) return null;
+            
+            return new InitialPosition(x, y, angle);
+        }
+
+        private static double? ParseDouble(string str)
+        {
+            if (str == null) return null;
+            try
+            {
+                return double.Parse(str.Trim(), CultureInfo.InvariantCulture);
+            }
+            catch (FormatException)
+            {
+                return null;
+            }
+        }
+    }
+}
