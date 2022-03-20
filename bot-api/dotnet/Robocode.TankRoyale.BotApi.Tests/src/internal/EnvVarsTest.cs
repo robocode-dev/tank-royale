1a5767b371a5eae0064adea2dbe71a8dbbc5aa74
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-03-20 23:45:02 +0100

    Added EnvVarsTest.cs

---
+++ bot-api/dotnet/Robocode.TankRoyale.BotApi.Tests/src/internal/EnvVarsTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Robocode.TankRoyale.BotApi.Internal;
+using Robocode.TankRoyale.BotApi.Tests.Test_utils;
+
+namespace Robocode.TankRoyale.BotApi.Tests.Internal
+{
+    public class EnvVarsTest
+    {
+        [Test]
+        public void GetBotInfo_ShouldWorkWhenAllEnvVarsAreSetCorrectly()
+        {
+            EnvironmentVariablesUtil.SetAll();
+            var info = EnvVars.GetBotInfo();
+        }
+    }
+}
