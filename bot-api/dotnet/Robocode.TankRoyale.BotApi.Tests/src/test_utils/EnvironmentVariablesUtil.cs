e5b99904177ecae167f091bbf86d39ec8d40fac2
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-03-21 00:08:36 +0100

    Rollback.

---
+++ bot-api/dotnet/Robocode.TankRoyale.BotApi.Tests/src/test_utils/EnvironmentVariablesUtil.cs
@@ -0,0 +1,28 @@
+using System;
+using static Robocode.TankRoyale.BotApi.Tests.Test_utils.EnvironmentVariablesConstants;
+
+namespace Robocode.TankRoyale.BotApi.Tests.Test_utils
+{
+    public static class EnvironmentVariablesUtil
+    {
+        public static void SetAll()
+        {
+            SetVar(ServerUrl, "ws://localhost:7654");
+            SetVar(BotName, "MyBot");
+            SetVar(BotVersion, "MyBot");
+            SetVar(BotAuthors, "Author1, Author2");
+            SetVar(BotGameTypes, "classic, melee, 1v1");
+            SetVar(BotDescription, "Short description");
+            SetVar(BotHomepage, "https://somewhere.net/MyBot");
+            SetVar(BotCountryCodes, "uk, us");
+            SetVar(BotPlatform, ".Net 5");
+            SetVar(BotProgrammingLang, "C# 8");
+            SetVar(BotInitialPosition, "50,70, 270");
+        }
+
+        private static void SetVar(string name, string value)
+        {
+            Environment.SetEnvironmentVariable(name, value);
+        }
+    }
+}
