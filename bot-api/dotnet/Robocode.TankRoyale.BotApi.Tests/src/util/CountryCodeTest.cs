45e356b5841210400c8329dad48092adb5ff3dea
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-03-22 22:26:30 +0100

    Added CountryCode.cs utility with unit test with a method for checking if a country code is valid.

---
+++ bot-api/dotnet/Robocode.TankRoyale.BotApi.Tests/src/util/CountryCodeTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Robocode.TankRoyale.BotApi.Util;
+
+namespace Robocode.TankRoyale.BotApi.Tests.Util
+{
+    public class CountryCodeTest
+    {
+        [Test]
+        [TestCase("uk")]
+        [TestCase("UK")]
+        [TestCase("GB")]
+        [TestCase("gb")]
+        [TestCase("NL")]
+        [TestCase("nl")]
+        [TestCase("DK")]
+        [TestCase("dk")]
+        [TestCase("US")]
+        [TestCase("us")]
+        public void IsCountryCodeValid_ShouldWork(string countryCode)
+        {
+            Assert.That(CountryCode.IsCountryCodeValid(countryCode), Is.True);
+        } 
+    }
+}
---
+++ bot-api/dotnet/Robocode.TankRoyale.BotApi/src/util/CountryCode.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Linq;
+
+namespace Robocode.TankRoyale.BotApi.Util
+{
+    public static class CountryCode
+    {
+        public static bool IsCountryCodeValid(string countryCode)
+        {
+            if (string.IsNullOrWhiteSpace(countryCode)) return false;
+            if (!(countryCode is {Length: 2})) return false;
+            if (countryCode.ToUpper() == "UK") countryCode = "GB"; // UK is not supported in .Net, so convert it to GB
+
+            return CultureInfo.GetCultures(CultureTypes.SpecificCultures)
+                .Select(culture => new RegionInfo(culture.LCID))
+                .Any(region => region.TwoLetterISORegionName == countryCode.ToUpper());
+        }
+    }
+}
