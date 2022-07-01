92757d1bc6973990c5a71aa770428eeb0b9a0fff
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-07-01 23:46:24 +0200

    Bugfix: BaseBotInternals did call closedEvent.set() when disconnecting

--- bot-api/dotnet/Robocode.TankRoyale.BotApi/src/internal/BaseBotInternals.cs
+++ bot-api/dotnet/Robocode.TankRoyale.BotApi/src/internal/BaseBotInternals.cs
@@ -487,6 +487,8 @@ public sealed class BaseBotInternals
     {
         BotEventHandlers.FireDisconnectedEvent(
             new E.DisconnectedEvent(socket.ServerUri, remote, statusCode, reason));
+
+        closedEvent.Set();
     }
 
     private void HandleConnectionError(Exception cause)
