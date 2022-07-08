4f957238e3c126a0edd4a098548ac4902d2b8cee
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-07-08 23:22:24 +0200

    Documented MathUtil.java

--- bot-api/java/src/main/java/dev/robocode/tankroyale/botapi/internal/MathUtil.java
+++ bot-api/java/src/main/java/dev/robocode/tankroyale/botapi/internal/MathUtil.java
@@ -1,7 +1,22 @@
 package dev.robocode.tankroyale.botapi.internal;
 
+/**
+ * Math utility class.
+ */
 public final class MathUtil {
 
+    // Hides constructor
+    private MathUtil() {
+    }
+
+    /**
+     * Returns value clamped to the inclusive range of min and max.
+     *
+     * @param value is the value to be clamped.
+     * @param min   is the lower bound of the result.
+     * @param max   is the upper bound of the result.
+     * @return is the clamped value.
+     */
     public static double clamp(double value, double min, double max) {
         return Math.min(max, Math.max(min, value));
     }
