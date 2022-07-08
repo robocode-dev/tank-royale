e5957b71a3f9dd54455024b6175539bdfc4e9bec
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-07-08 23:21:28 +0200

    Added CollectionUtil

---
+++ bot-api/dotnet/Robocode.TankRoyale.BotApi/src/internal/CollectionUtil.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Robocode.TankRoyale.BotApi.Internal;
+
+/// <summary>
+/// Collection utility class.
+/// </summary>
+public static class CollectionUtil
+{
+    /// <summary>
+    /// Creates a mutable list that is a copy of another list.
+    /// </summary>
+    /// <param name="list">The list to copy, where <c>null</c> results in returning an empty mutable list.</param>
+    /// <returns>A mutable list that is a copy of the input list.</returns>
+    public static IList<T> ToMutableList<T>(IEnumerable<T> list) {
+        return list == null ? new List<T>() : new List<T>(list);
+    }
+
+    /// <summary>
+    /// Creates a mutable set that copies all items from a collection, but is removing duplicates.
+    /// </summary>
+    /// <param name="collection">The collection to copy, where <c>null</c> results in returning an empty mutable set.
+    /// </param>
+    /// <returns>A mutable set that is a copy of the input collection.</returns>
+    public static ISet<T> ToMutableSet<T>(ICollection<T> collection) {
+        return collection == null ? new HashSet<T>() : new HashSet<T>(collection);
+    } 
+}
---
+++ bot-api/java/src/main/java/dev/robocode/tankroyale/botapi/internal/CollectionUtil.java
@@ -0,0 +1,33 @@
+package dev.robocode.tankroyale.botapi.internal;
+
+import java.util.*;
+
+/**
+ * Collection utility class.
+ */
+public final class CollectionUtil {
+
+    // Hides constructor
+    private CollectionUtil() {
+    }
+
+    /**
+     * Creates a mutable list that is a copy of another list.
+     *
+     * @param list is the list to copy, where {@code null} results in returning an empty mutable list.
+     * @return a mutable list that is a copy of the input list.
+     */
+    public static <T> List<T> toMutableList(List<T> list) {
+        return list == null ? new ArrayList<>() : new ArrayList<>(list);
+    }
+
+    /**
+     * Creates a mutable set that copies all items from a collection, but is removing duplicates.
+     *
+     * @param collection is the collection to copy, where {@code null} results in returning an empty mutable set.
+     * @return a mutable set that is a copy of the input collection.
+     */
+    public static <T> Set<T> toMutableSet(Collection<T> collection) {
+        return collection == null ? new HashSet<>() : new HashSet<>(collection);
+    }
+}
