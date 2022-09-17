e4ffe7cc0c004ac4a4df303674cb8acb53b5029c
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-09-17 16:06:33 +0200

    Sound class was updated to play nothing, if the sound file is not available.

--- gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/audio/Sound.kt
+++ gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/audio/Sound.kt
@@ -9,16 +9,23 @@ class Sound(private val byteArray: ByteArray) {
 
     companion object {
         fun fromFile(filename: String): Sound {
-            val file = File(filename)
-            val fileInputStream = FileInputStream(file)
-            val byteArray = ByteArray(file.length().toInt())
-            fileInputStream.read(byteArray)
-            fileInputStream.close()
+            var byteArray: ByteArray
+            try {
+                val file = File(filename)
+                val fileInputStream = FileInputStream(file)
+                byteArray = ByteArray(file.length().toInt())
+                fileInputStream.read(byteArray)
+                fileInputStream.close()
+            } catch (ex: Exception) {
+                byteArray = ByteArray(0)
+            }
             return Sound(byteArray)
         }
     }
 
     fun play() {
+        if (byteArray.isEmpty()) return
+
         val inputStream = BufferedInputStream(ByteArrayInputStream(byteArray))
         val audioStream = AudioSystem.getAudioInputStream(inputStream)
         AudioSystem.getClip().apply {
