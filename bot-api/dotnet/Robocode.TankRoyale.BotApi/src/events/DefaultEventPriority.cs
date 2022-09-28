8743103a4f8adcd4a56d5a2b02c061a6a8534f28
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-09-28 20:32:35 +0200

    Flipped priority values, so lower priority values means higher priority precedence that is similar to the original Robocode.

--- bot-api/dotnet/Robocode.TankRoyale.BotApi/src/events/DefaultEventPriority.cs
+++ bot-api/dotnet/Robocode.TankRoyale.BotApi/src/events/DefaultEventPriority.cs
@@ -1,57 +1,30 @@
 namespace Robocode.TankRoyale.BotApi.Events;
 
 /// <summary>
-/// Default event priorities values. The higher value, the higher event priority. So the <see cref="WonRoundEvent"/>
-/// has the highest priority (150), and <see cref="DeathEvent"/> has the lowest priority (10).
+/// Default event priorities values. The lower value, the higher event priority. So the <see cref="DeathEvent"/>
+/// has the highest priority (10), and <see cref="WonRoundEvent"/> has the lowest priority (150).
 /// </summary>
 public static class DefaultEventPriority
 {
     /// <summary>
-    /// Event priority for the <see cref="WonRoundEvent"/>
+    /// Event priority for the <see cref="DeathEvent"/>
     /// </summary>
-    public const int WonRound = 150;
-
-    /// <summary>
-    /// Event priority for the <see cref="SkippedTurnEvent"/>
-    /// </summary>
-    public const int SkippedTurn = 140;
+    public const int Death = 10;
     
     /// <summary>
-    /// Event priority for the <see cref="TickEvent"/>
+    /// Event priority for the <see cref="ScannedBotEvent"/>
     /// </summary>
-    public const int Tick = 130;
+    public const int ScannedBot = 20;
 
     /// <summary>
-    /// Event priority for the <see cref="CustomEvent"/>
+    /// Event priority for the <see cref="HitBotEvent"/>
     /// </summary>
-    public const int Custom = 120;
+    public const int HitBot = 30;
     
-    //  public const int TeamMessage = 110; // Reserved for future
-
     /// <summary>
-    /// Event priority for the <see cref="BotDeathEvent"/>
+    /// Event priority for the <see cref="HitWallEvent"/>
     /// </summary>
-    public const int BotDeath = 100;
-
-    /// <summary>
-    /// Event priority for the <see cref="BulletHitWallEvent"/>
-    /// </summary>
-    public const int BulletHitWall = 90;
-
-    /// <summary>
-    /// Event priority for the <see cref="BulletHitBulletEvent"/>
-    /// </summary>
-    public const int BulletHitBullet = 80;
-
-    /// <summary>
-    /// Event priority for the <see cref="BulletHitBotEvent"/>
-    /// </summary>
-    public const int BulletHitBot = 70;
-
-    /// <summary>
-    /// Event priority for the <see cref="BulletFiredEvent"/>
-    /// </summary>
-    public const int BulletFired = 60;
+    public const int HitWall = 40;
 
     /// <summary>
     /// Event priority for the <see cref="HitByBulletEvent"/>
@@ -59,22 +32,52 @@ public static class DefaultEventPriority
     public const int HitByBullet = 50;
 
     /// <summary>
-    /// Event priority for the <see cref="HitWallEvent"/>
+    /// Event priority for the <see cref="BulletFiredEvent"/>
     /// </summary>
-    public const int HitWall = 40;
+    public const int BulletFired = 60;
 
     /// <summary>
-    /// Event priority for the <see cref="HitBotEvent"/>
+    /// Event priority for the <see cref="BulletHitBotEvent"/>
     /// </summary>
-    public const int HitBot = 30;
+    public const int BulletHitBot = 70;
 
     /// <summary>
-    /// Event priority for the <see cref="ScannedBotEvent"/>
+    /// Event priority for the <see cref="BulletHitBulletEvent"/>
     /// </summary>
-    public const int ScannedBot = 20;
+    public const int BulletHitBullet = 80;
 
     /// <summary>
-    /// Event priority for the <see cref="DeathEvent"/>
+    /// Event priority for the <see cref="BulletHitWallEvent"/>
     /// </summary>
-    public const int Death = 10;
+    public const int BulletHitWall = 90;
+
+    /// <summary>
+    /// Event priority for the <see cref="BotDeathEvent"/>
+    /// </summary>
+    public const int BotDeath = 100;
+
+    /// <summary>
+    /// Reserved event priority for the future TeamMessageEvent.
+    /// </summary>
+    public const int TeamMessage = 110; // Reserved for future
+
+    /// <summary>
+    /// Event priority for the <see cref="CustomEvent"/>
+    /// </summary>
+    public const int Custom = 120;
+
+    /// <summary>
+    /// Event priority for the <see cref="TickEvent"/>
+    /// </summary>
+    public const int Tick = 130;
+    
+    /// <summary>
+    /// Event priority for the <see cref="SkippedTurnEvent"/>
+    /// </summary>
+    public const int SkippedTurn = 140;
+
+    /// <summary>
+    /// Event priority for the <see cref="WonRoundEvent"/>
+    /// </summary>
+    public const int WonRound = 150;
 }
