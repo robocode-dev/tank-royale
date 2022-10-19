8d3cbb0cf531623d451e585d1f3e9172d754657e
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-10-19 22:01:05 +0200

    Improved CountryCodeTest

--- bot-api/dotnet/Robocode.TankRoyale.BotApi.Tests/src/util/CountryCodeTest.cs
+++ bot-api/dotnet/Robocode.TankRoyale.BotApi.Tests/src/util/CountryCodeTest.cs
@@ -1,28 +1,27 @@
 ﻿using NUnit.Framework;
 using Robocode.TankRoyale.BotApi.Util;
 
-namespace Robocode.TankRoyale.BotApi.Tests.Util
-{
-    public class CountryCodeTest
-    {
-        [Test]
-        [TestCase("GB")]
-        [TestCase("gb")]
-        [TestCase("dk")]
-        [TestCase("us")]
-        [TestCase("no")]
-        [TestCase("SE")]
-        [TestCase("FI")]
-        public void IsCountryCodeValid_ReturnsTrueForGivenCountryCodes(string countryCode)
-        {
-            Assert.That(CountryCode.IsCountryCodeValid(countryCode), Is.True);
-        }
+namespace Robocode.TankRoyale.BotApi.Tests.Util;
 
-        [Test]
-        public void GetLocalCountryCode_ReturnValidCountryCode()
-        {
-            var countryCode = CountryCode.GetLocalCountryCode();
-            Assert.That(CountryCode.IsCountryCodeValid(countryCode), Is.True);
-        }
+public class CountryCodeTest
+{
+    [Test]
+    [TestCase("GB")]
+    [TestCase("gb")]
+    [TestCase("dk")]
+    [TestCase("us")]
+    [TestCase("no")]
+    [TestCase("SE")]
+    [TestCase("FI")]
+    public void GivenValidCountryCodes_whenCallingIsCountryCodeValid_thenReturnTrue(string countryCode)
+    {
+        Assert.That(CountryCode.IsCountryCodeValid(countryCode), Is.True);
+    }
+
+    [Test]
+    public void GivenLocalCountryCode_whenCallingIsCountryCodeValid_thenReturnTrue()
+    {
+        var countryCode = CountryCode.GetLocalCountryCode();
+        Assert.That(CountryCode.IsCountryCodeValid(countryCode), Is.True);
     }
 }
