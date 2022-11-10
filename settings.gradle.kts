8e544d2ed7493a719e07adbe1fc5b8a2109d6585
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2022-11-10 09:23:45 +0100

    Upgraded dependencies + rolled to version 0.17.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-val version = "0.17.4"
+val version = "0.17.5" // <-- The current version of Robocode Tank Royale
 
 // Schema Generator
 include("schema:jvm")
@@ -24,7 +24,7 @@ include("sample-bots:csharp")
 // Docs
 include("buildDocs")
 
-val kotlinVersion = "1.7.20"
+val kotlinVersion = "1.7.21"
 val junitVersion = "5.9.1"
 
 // Check dependencies with this command:  gradle dependencyUpdates
@@ -35,16 +35,16 @@ dependencyResolutionManagement {
             version("tankroyale", version)
             version("node", "15.5.1")
 
-            library("gson", "com.google.code.gson:gson:2.9.1")
-            library("gson-extras", "org.danilopianini:gson-extras:1.0.0")
+            library("gson", "com.google.code.gson:gson:2.10")
+            library("gson-extras", "org.danilopianini:gson-extras:1.1.0")
             library("jansi", "org.fusesource.jansi:jansi:2.4.0")
             library("java-websocket", "org.java-websocket:Java-WebSocket:1.5.3")
             library("jsonschema2pojo", "org.jsonschema2pojo:jsonschema2pojo-gradle-plugin:1.1.2")
-            library("picocli", "info.picocli:picocli:4.6.3")
+            library("picocli", "info.picocli:picocli:4.7.0")
             library("proguard-gradle", "com.guardsquare:proguard-gradle:7.2.2")
             library("miglayout-swing", "com.miglayout:miglayout-swing:11.0")
             library("nv-i18n", "com.neovisionaries:nv-i18n:1.29")
-            library("serialization-json", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+            library("serialization-json", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
             library("slf4j-simple", "org.slf4j:slf4j-simple:2.0.3")
 
             // Java testing
@@ -52,19 +52,21 @@ dependencyResolutionManagement {
             library("junit-engine", "org.junit.jupiter:junit-jupiter-engine:$junitVersion")
             library("junit-params", "org.junit.jupiter:junit-jupiter-params:$junitVersion")
             library("assertj", "org.assertj:assertj-core:3.23.1")
-            library("junit-pioneer", "org.junit-pioneer:junit-pioneer:1.7.1")
+            library("junit-pioneer", "org.junit-pioneer:junit-pioneer:1.7.2")
 
             // Kotlin testing
-            library("kotest-junit5", "io.kotest:kotest-runner-junit5-jvm:5.4.2")
-            library("mockk", "io.mockk:mockk:1.13.1")
+            library("kotest-junit5", "io.kotest:kotest-runner-junit5-jvm:5.5.4")
+            library("mockk", "io.mockk:mockk:1.13.2")
 
             plugin("kotlin-jvm", "org.jetbrains.kotlin.jvm").version(kotlinVersion)
             plugin("kotlin-serialization", "org.jetbrains.kotlin.plugin.serialization").version(kotlinVersion)
             plugin("nexus-publish", "io.github.gradle-nexus.publish-plugin").version("1.1.0")
             plugin("shadow-jar","com.github.johnrengelman.shadow").version("7.1.2")
-            plugin("itiviti-dotnet", "com.itiviti.dotnet").version("1.9.3")
-            plugin("node-gradle", "com.github.node-gradle.node").version("3.4.0")
-            plugin("benmanes-versioning", "com.github.ben-manes.versions").version("0.42.0")
+            plugin("itiviti-dotnet", "com.itiviti.dotnet").version("1.9.3") // 1.9.4 causes MSB1006 issue
+            plugin("node-gradle", "com.github.node-gradle.node").version("3.5.0")
+
+            // Dependencies versions
+            plugin("benmanes-versioning", "com.github.ben-manes.versions").version("0.43.0")
         }
     }
 }
