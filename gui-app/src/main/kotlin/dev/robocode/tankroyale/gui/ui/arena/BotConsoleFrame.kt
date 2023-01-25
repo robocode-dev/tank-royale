2a4285fcde7a369a00f4360fc35d7677bd563c59
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2023-01-25 22:38:12 +0100

    Bug fix: Only one bot console window was writing out data.

--- gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/ui/arena/BotConsoleFrame.kt
+++ gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/ui/arena/BotConsoleFrame.kt
@@ -16,19 +16,25 @@ import javax.swing.text.html.HTMLEditorKit
 class BotConsoleFrame(bot: Participant, frameCounter: Int = 0) :
     ConsoleFrame(bot.displayName, isTitlePropertyName = false) {
 
-    private val onOk = Event<JButton>().apply { subscribe(this) { dispose() } }
-    private val onClear = Event<JButton>().apply { subscribe(this) { clear() } }
-
-    private val editorKit = HTMLEditorKit().apply {
-        styleSheet.addRule("body { color: white; font-family: monospace; }")
-        styleSheet.addRule(".error { color: red; }")
+    companion object {
+        private val editorKit = HTMLEditorKit().apply {
+            styleSheet.addRule("body { color: white; font-family: monospace; }")
+            styleSheet.addRule(".error { color: \"#FF5733\"; }") // dark pink
+            styleSheet.addRule(".linenumber { color: gray; }")
+            styleSheet.addRule(".round-info { color: \"#377B37\"; }") // olive green
+        }
     }
 
     private val document = editorKit.createDefaultDocument() as HTMLDocument
 
+    private var numberOfRounds: Int = 0
+
+    private val onOk = Event<JButton>().apply { subscribe(this) { dispose() } }
+    private val onClear = Event<JButton>().apply { subscribe(this) { clear() } }
+
     init {
-        setLocation(10, 10 + frameCounter * 50)
-        setSize(400, 300)
+        setLocation(10, 10 + frameCounter * 50) // increment y for each bot console frame
+        setSize(600, 400)
 
         editorPane.contentType = "text/html"
         editorPane.editorKit = editorKit
@@ -41,25 +47,44 @@ class BotConsoleFrame(bot: Participant, frameCounter: Int = 0) :
 
         contentPane.add(buttonPanel, BorderLayout.SOUTH)
 
+        ClientEvents.onGameStarted.subscribe(this) {
+            numberOfRounds = it.gameSetup.numberOfRounds
+        }
+        ClientEvents.onRoundStarted.subscribe(this) {
+            updateRoundInfo(it.roundNumber)
+        }
         ClientEvents.onTickEvent.subscribe(this) { tickEvent ->
             run {
                 val botState = tickEvent.botStates.filter { it.id == bot.id }[0]
-                updateBotState(botState)
+                updateBotState(botState, tickEvent.turnNumber)
             }
         }
     }
 
-    private fun updateBotState(botState: BotState) {
-        appendText(botState.stdOut)
-        appendText(botState.stdErr, "error")
+    private fun updateRoundInfo(roundNumber: Int) {
+        appendText(
+            """
+            ====================<br>
+            Round $roundNumber of $numberOfRounds<br>
+            ====================<br>
+        """.trimIndent(), "round-info"
+        )
     }
 
-    private fun appendText(str: String?, cssClass: String? = null) {
-        var html = str
+    private fun updateBotState(botState: BotState, turnNumber: Int) {
+        appendText(botState.stdOut, turnNumber = turnNumber)
+        appendText(botState.stdErr, "error", turnNumber)
+    }
+
+    private fun appendText(text: String?, cssClass: String? = null, turnNumber: Int? = null) {
+        var html = text
         if (html != null) {
             html = html.replace("\\n", "<br>")
             if (cssClass != null) {
                 html = "<span class=\"$cssClass\">$html</span>"
+            }
+            if (turnNumber != null) {
+                html = "<span class=\"linenumber\">$turnNumber:</span> $html"
             }
             editorKit.insertHTML(document, document.length, html, 0, 0, null)
         }
