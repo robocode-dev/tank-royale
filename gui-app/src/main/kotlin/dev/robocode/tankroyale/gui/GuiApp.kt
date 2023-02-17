15e6df4176f047f5613a72e2d6582c9cdf74d38e
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2023-02-17 22:54:47 +0100

    Direct3D and OpenGL acceleration is now disabled under Windows.

--- gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/GuiApp.kt
+++ gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/GuiApp.kt
@@ -3,7 +3,6 @@ package dev.robocode.tankroyale.gui
 import dev.robocode.tankroyale.gui.ui.MainFrame
 import dev.robocode.tankroyale.gui.ui.components.Images
 import java.awt.Taskbar
-import java.util.*
 import javax.swing.UIManager
 
 fun main() {
@@ -19,11 +18,10 @@ fun main() {
 }
 
 private fun fixRenderingIssues() {
-    val osName = System.getProperty("os.name", "generic").lowercase(Locale.ENGLISH)
-    val isMac = osName.contains("mac") || osName.contains("darwin")
-    if (!isMac) {
-        System.setProperty("sun.java2d.d3d", "false") // turn off use of Direct3D
-        System.setProperty("sun.java2d.noddraw", "true") // no use of Direct Draw
-        System.setProperty("sun.java2d.opengl", "true") // turn on use of OpenGL
+    val osName = System.getProperty("os.name", "generic")
+    if (osName.startsWith("windows", ignoreCase = true)) {
+        // Disable hardware acceleration to avoid issue with rendering
+        System.setProperty("sun.java2d.d3d", "false") // disable Direct3D acceleration
+        System.setProperty("sun.java2d.opengl", "false") // disable OpenGL acceleration
     }
 }
