51712413ee40af4044c75941c7ba87b708bf1d8f
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2023-08-08 15:35:20 +0200

    Fixed redundant/dangling event subscriptions to WebSocket client for the GUI application, when connection failed.

--- gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/util/BusyCursor.kt
+++ gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/util/BusyCursor.kt
@@ -1,19 +1,36 @@
 package dev.robocode.tankroyale.gui.util
 
 import dev.robocode.tankroyale.gui.ui.MainFrame
+import java.awt.Color
 import java.awt.Cursor
 import java.util.concurrent.atomic.AtomicInteger
+import javax.swing.JFrame
 
 object BusyCursor {
 
     private var count = AtomicInteger(0)
 
+    private val invisibleFrame = JFrame().apply {
+        cursor = Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR)
+        isAlwaysOnTop = true
+        isUndecorated = true
+        isVisible = true
+
+        background = Color(0, 0, 0, 0)
+    }
+
     fun activate() {
-        count.incrementAndGet()
-        MainFrame.cursor = Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR)
+        if (count.getAndIncrement() == 0) {
+            invisibleFrame.apply {
+                bounds = MainFrame.bounds
+                invisibleFrame.isVisible = true
+            }
+        }
     }
 
     fun deactivate() {
-        if (count.decrementAndGet() == 0) MainFrame.cursor = Cursor.getDefaultCursor()
+        if (count.decrementAndGet() == 0) {
+            invisibleFrame.isVisible = false
+        }
     }
 }
