78591d445eb67eae6966278ff4fb000bae8f6b6a
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2024-01-07 12:22:25 +0100

    Microsoft.Extensions.Configuration.Binder -> 8.0.0

--- buildSrc/src/main/kotlin/build/csproj/BotCsprojFileGenerator.kt
+++ buildSrc/src/main/kotlin/build/csproj/BotCsprojFileGenerator.kt
@@ -49,7 +49,7 @@ private fun writeXml(out: OutputStream, botName: String, botApiVersion: String) 
                 }
                 element("PackageReference") {
                     attribute("Include", "Microsoft.Extensions.Configuration.Binder")
-                    attribute("Version", "7.0.1")
+                    attribute("Version", "8.0.0")
                 }
             }
         }
