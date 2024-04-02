94dce124755e72e980a5982da4cc71569b61e332
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2024-04-02 22:32:23 +0200

    Removal of unused methods

--- gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/model/InitialPosition.kt
+++ gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/model/InitialPosition.kt
@@ -3,37 +3,4 @@ package dev.robocode.tankroyale.gui.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class InitialPosition(
-    val x: Double?, val y: Double?, val direction: Double?) {
-
-    companion object {
-        fun fromString(initialPosition: String?): InitialPosition? {
-            if (initialPosition == null || initialPosition.isBlank()) return null
-            val values = initialPosition.trim().split("\\s*,\\s*|\\s+".toRegex()).toTypedArray()
-            return parseInitialPosition(values)
-        }
-
-        private fun parseInitialPosition(values: Array<String>): InitialPosition? {
-            if (values.isEmpty()) return null
-            val x: Double? = parseDouble(values[0])
-            if (values.size < 2) {
-                return InitialPosition(x, null, null)
-            }
-            val y: Double? = parseDouble(values[1])
-            var angle: Double? = null
-            if (values.size >= 3) {
-                angle = parseDouble(values[2])
-            }
-            return InitialPosition(x, y, angle)
-        }
-
-        private fun parseDouble(str: String?): Double? {
-            return if (str == null) null
-            else try {
-                str.trim().toDouble()
-            } catch (ex: NumberFormatException) {
-                null
-            }
-        }
-    }
-}
+data class InitialPosition(val x: Double?, val y: Double?, val direction: Double?)
