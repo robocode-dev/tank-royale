d1f03086602130631389a87ceca134ad8c503b38
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2024-06-09 22:28:34 +0200

    Use isEmpty()

--- bot-api/java/src/test/java/dev/robocode/tankroyale/botapi/util/JsonUtilTest.java
+++ bot-api/java/src/test/java/dev/robocode/tankroyale/botapi/util/JsonUtilTest.java
@@ -24,7 +24,7 @@ class JsonUtilTest {
 
     @Test
     void testEscapedCarriageReturn() {
-        assertThat(escaped("\r")).isEqualTo("");
+        assertThat(escaped("\r")).isEmpty();
     }
 
     @Test
