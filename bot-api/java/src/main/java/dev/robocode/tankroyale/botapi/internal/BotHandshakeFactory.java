8c642502847f6e9966f8d7905e6575317eba1807
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2024-06-09 12:03:20 +0200

    Hide constructor

--- bot-api/java/src/main/java/dev/robocode/tankroyale/botapi/internal/BotHandshakeFactory.java
+++ bot-api/java/src/main/java/dev/robocode/tankroyale/botapi/internal/BotHandshakeFactory.java
@@ -3,6 +3,7 @@ package dev.robocode.tankroyale.botapi.internal;
 import dev.robocode.tankroyale.botapi.BotInfo;
 import dev.robocode.tankroyale.botapi.mapper.InitialPositionMapper;
 import dev.robocode.tankroyale.schema.BotHandshake;
+import dev.robocode.tankroyale.schema.Message.Type;
 
 import java.util.ArrayList;
 
@@ -11,10 +12,14 @@ import java.util.ArrayList;
  */
 final class BotHandshakeFactory {
 
+    // Hide constructor as this class is a utility class used for static access only
+    private BotHandshakeFactory() {
+    }
+
     static BotHandshake create(String sessionId, BotInfo botInfo, boolean isDroid, String secret) {
         BotHandshake handshake = new BotHandshake();
         handshake.setSessionId(sessionId);
-        handshake.setType(BotHandshake.Type.BOT_HANDSHAKE);
+        handshake.setType(Type.BOT_HANDSHAKE);
         handshake.setName(botInfo.getName());
         handshake.setVersion(botInfo.getVersion());
         handshake.setAuthors(new ArrayList<>(botInfo.getAuthors()));
