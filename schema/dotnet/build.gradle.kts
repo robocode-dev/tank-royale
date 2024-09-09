d83e3fa934a7275f48df68ea0bbfa67f71ac175b
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2024-09-09 23:14:27 +0200

    Generate the Gfx schemas for .Net

--- schema/dotnet/build.gradle.kts
+++ schema/dotnet/build.gradle.kts
@@ -14,22 +14,26 @@ tasks {
 
         doFirst {
             mkdir("${project(":bot-api:dotnet").file("Robocode.TankRoyale.BotApi/src/generated/Game")}")
+            mkdir("${project(":bot-api:dotnet").file("Robocode.TankRoyale.BotApi/src/generated/Gfx")}")
+
+            var codeGeneratorPath = "$projectDir/bin/Release/net6.0/CodeGeneratorApp"
+            if (System.getProperty("os.name").lowercase().contains("windows")) {
+                codeGeneratorPath += ".exe"
+            }
+
+            commandLine(
+                codeGeneratorPath,
+                "${project(":schema").file("schemas/game")}",
+                "${project(":bot-api:dotnet").file("Robocode.TankRoyale.BotApi/src/generated/Game")}",
+                "Robocode.TankRoyale.Schema.Game"
+            )
+
+            commandLine(
+                codeGeneratorPath,
+                "${project(":schema").file("schemas/gfx")}",
+                "${project(":bot-api:dotnet").file("Robocode.TankRoyale.BotApi/src/generated/Gfx")}",
+                "Robocode.TankRoyale.Schema.Gfx"
+            )
         }
-
-        var codeGeneratorPath = "$projectDir/bin/Release/net6.0/CodeGeneratorApp"
-        if (System.getProperty("os.name").lowercase().contains("windows")) {
-            codeGeneratorPath += ".exe"
-        }
-
-        println("codeGeneratorPath: " + codeGeneratorPath)
-        println("${project(":schema").file("schemas")}")
-        println("${project(":bot-api:dotnet").file("Robocode.TankRoyale.BotApi/src/generated/Game")}")
-
-        commandLine(
-            codeGeneratorPath,
-            "${project(":schema").file("schemas/game")}",
-            "${project(":bot-api:dotnet").file("Robocode.TankRoyale.BotApi/src/generated/Game")}",
-            "Robocode.TankRoyale.Schema.Game"
-        )
     }
 }
