c9d48ede6f82f5e3fb622b4d0513d1c8ffbbd07e
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2024-10-03 17:47:31 +0200

    Improved JComponentExt extension class

--- gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/ui/extensions/JComponentExt.kt
+++ gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/ui/extensions/JComponentExt.kt
@@ -8,47 +8,92 @@ import java.awt.Container
 import java.awt.EventQueue
 import javax.swing.*
 
-
 object JComponentExt {
 
-    fun JComponent.addLabel(stringResourceName: String, layoutConstraints: String? = null): JLabel {
-        val label = object : JLabel(Strings.get(stringResourceName) + ':') {
+    fun createLabel(stringResourceName: String) =
+        object : JLabel(Strings.get(stringResourceName)) {
             override fun createToolTip() = RcToolTip()
         }
+
+    fun JComponent.addLabel(stringResourceName: String, layoutConstraints: String? = null): JLabel {
+        val label = createLabel(stringResourceName)
         add(label, layoutConstraints)
         return label
     }
+
+    fun createButton(stringResourceName: String, event: Event<JButton>): JButton =
+        object : JButton(Strings.get(stringResourceName)) {
+            override fun createToolTip() = RcToolTip()
+        }.apply {
+            addActionListener { event.fire(this) }
+        }
 
     fun JComponent.addButton(
         stringResourceName: String,
         event: Event<JButton>,
         layoutConstraints: String? = null
     ): JButton {
-        val button = object : JButton(Strings.get(stringResourceName)) {
-            override fun createToolTip() = RcToolTip()
-        }
-        button.addActionListener { event.fire(button) }
+        val button = createButton(stringResourceName, event)
         add(button, layoutConstraints)
         return button
     }
 
+    fun createOkButton(event: Event<JButton>) = createButton("ok", event)
+
     fun JComponent.addOkButton(event: Event<JButton>, layoutConstraints: String? = null): JButton {
-        return addButton("ok", event, layoutConstraints)
+        val okButton = createOkButton(event)
+        add(okButton, layoutConstraints)
+        return okButton
     }
+
+    fun createCancelButton(event: Event<JButton>) = createButton("cancel", event)
 
     fun JComponent.addCancelButton(event: Event<JButton>, layoutConstraints: String? = null): JButton {
-        return addButton("cancel", event, layoutConstraints)
+        val cancelButton = createCancelButton(event)
+        add(cancelButton, layoutConstraints)
+        return cancelButton
     }
 
-    fun JComponent.addCheckBox(
-        stringResourceName: String, event: Event<JCheckBox>, layoutConstraints: String? = null
-    ): JCheckBox {
-        val checkbox = object: JCheckBox(Strings.get(stringResourceName)) {
+    fun createAddButton(event: Event<JButton>) = createButton("add", event)
+
+    fun JComponent.addAddButton(event: Event<JButton>, layoutConstraints: String? = null): JButton {
+        val addButton = createAddButton(event)
+        add(addButton, layoutConstraints)
+        return addButton
+    }
+
+    fun createRemoveButton(event: Event<JButton>) = createButton("remove", event)
+
+    fun JComponent.addRemoveButton(event: Event<JButton>, layoutConstraints: String? = null): JButton {
+        val removeButton = createRemoveButton(event)
+        add(removeButton, layoutConstraints)
+        return removeButton
+    }
+
+    fun createEditButton(event: Event<JButton>) = createButton("edit", event)
+
+    fun JComponent.addEditButton(event: Event<JButton>, layoutConstraints: String? = null): JButton {
+        val editButton = createEditButton(event)
+        add(editButton, layoutConstraints)
+        return editButton
+    }
+
+    fun JComponent.createCheckBox(stringResourceName: String, event: Event<JCheckBox>) =
+        object : JCheckBox(Strings.get(stringResourceName)) {
             override fun createToolTip() = RcToolTip()
+        }.apply {
+            addActionListener { event.fire(this) }
         }
-        checkbox.addActionListener { event.fire(checkbox) }
-        add(checkbox, layoutConstraints)
-        return checkbox
+
+    fun JComponent.addCheckBox(
+        stringResourceName: String,
+        event: Event<JCheckBox>,
+        layoutConstraints: String?
+    )
+            : JCheckBox {
+        val checkBox = createCheckBox(stringResourceName, event)
+        add(checkBox, layoutConstraints)
+        return checkBox
     }
 
     fun JComponent.setDefaultButton(button: JButton) {
@@ -73,8 +118,7 @@ object JComponentExt {
     }
 
     private fun enableAll(container: Container, enable: Boolean) {
-        val components = container.components
-        for (component in components) {
+        container.components.forEach { component ->
             component.isEnabled = enable
             if (component is Container) {
                 enableAll(component, enable)
