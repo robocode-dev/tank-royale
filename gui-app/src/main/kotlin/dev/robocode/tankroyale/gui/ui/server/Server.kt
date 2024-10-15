ee7eaaff750afa7484af993b91b789cad84b4c90
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2024-10-15 21:43:22 +0200

    Extended the Server class to be able to connect to a remote server besides a local server.

--- gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/ui/server/Server.kt
+++ gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/ui/server/Server.kt
@@ -3,8 +3,9 @@ package dev.robocode.tankroyale.gui.ui.server
 import dev.robocode.tankroyale.gui.client.Client
 import dev.robocode.tankroyale.gui.client.ClientEvents
 import dev.robocode.tankroyale.gui.server.ServerProcess
+import dev.robocode.tankroyale.gui.settings.ServerSettings
 import dev.robocode.tankroyale.gui.ui.Messages
-import dev.robocode.tankroyale.gui.util.MessageDialog.showConfirm
+import dev.robocode.tankroyale.gui.util.MessageDialog
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
@@ -12,23 +13,40 @@ object Server {
 
     fun isRunning() = ServerProcess.isRunning() || RemoteServer.isRunning()
 
-    fun connectOrStart(): Boolean /* started */ {
+    fun connectOrStart(): Boolean /* connected and started */ {
         try {
-            if (Client.isGameRunning()) {
-                if (!showConfirm(Messages.get("stop_battle"))) {
-                    return false // not started
-                }
-                Client.stopGame()
-            }
-            if (!isRunning()) {
-                start()
-            }
-            connectToServer()
-            return true // started
+            if (ServerSettings.useRemoteServer) {
+                // use remote server
 
+                if (RemoteServer.isRunning()) {
+                    connectToServer()
+                    return true // connected
+                } else {
+                    val message = String.format(
+                        Messages.get("cannot_connect_to_remote_server"),
+                        ServerSettings.useRemoteServerUrl
+                    )
+                    MessageDialog.showError(message)
+                    return false
+                }
+            } else {
+                // use local server
+
+                if (Client.isGameRunning()) {
+                    if (!MessageDialog.showConfirm(Messages.get("stop_battle"))) {
+                        return false // not started
+                    }
+                    Client.stopGame()
+                }
+                if (!isRunning()) {
+                    start()
+                }
+                connectToServer()
+                return true // connected
+            }
         } catch (e: Exception) {
             System.err.println(e.message)
-            return false // not started
+            return false // not connected
         }
     }
 
