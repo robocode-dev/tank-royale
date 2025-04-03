fdbb6d4b244665365fddfb088a3ab61f619663be
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2025-04-04 00:33:50 +0200

    Added __init__.py for the event classes

---
+++ bot-api/python/src/robocode_tank_royale/bot_api/events/__init__.py
@@ -0,0 +1,56 @@
+# Event classes
+from .bot_death_event import BotDeathEvent
+from .bot_event import BotEvent
+from .bullet_fired_event import BulletFiredEvent
+from .bullet_hit_bot_event import BulletHitBotEvent
+from .bullet_hit_bullet_event import BulletHitBulletEvent
+from .bullet_hit_wall_event import BulletHitWallEvent
+from .condition import Condition
+from .connected_event import ConnectedEvent
+from .connection_error_event import ConnectionErrorEvent
+from .connection_event import ConnectionEvent
+from .custom_event import CustomEvent
+from .death_event import DeathEvent
+from .disconnected_event import DisconnectedEvent
+from .event_abc import EventABC
+from .game_ended_event import GameEndedEvent
+from .game_started_event import GameStartedEvent
+from .hit_bot_event import HitBotEvent
+from .hit_by_bullet_event import HitByBulletEvent
+from .hit_wall_event import HitWallEvent
+from .round_ended_event import RoundEndedEvent
+from .round_started_event import RoundStartedEvent
+from .scanned_bot_event import ScannedBotEvent
+from .skipped_turn_event import SkippedTurnEvent
+from .team_message_event import TeamMessageEvent
+from .tick_event import TickEvent
+from .won_round_event import WonRoundEvent
+
+__all__ = [
+    'BotDeathEvent',
+    'BotEvent',
+    'BulletFiredEvent',
+    'BulletHitBotEvent',
+    'BulletHitBulletEvent',
+    'BulletHitWallEvent',
+    'Condition',
+    'ConnectedEvent',
+    'ConnectionErrorEvent',
+    'ConnectionEvent',
+    'CustomEvent',
+    'DeathEvent',
+    'DisconnectedEvent',
+    'EventABC',
+    'GameEndedEvent',
+    'GameStartedEvent',
+    'HitBotEvent',
+    'HitByBulletEvent',
+    'HitWallEvent',
+    'RoundEndedEvent',
+    'RoundStartedEvent',
+    'ScannedBotEvent',
+    'SkippedTurnEvent',
+    'TeamMessageEvent',
+    'TickEvent',
+    'WonRoundEvent'
+]
