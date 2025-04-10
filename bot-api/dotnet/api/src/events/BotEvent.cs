7fd7b23957dfbeade4ff9caf6974d44152517b82
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2025-04-10 22:16:05 +0200

    Added missing XML comments

--- bot-api/dotnet/api/src/events/BotEvent.cs
+++ bot-api/dotnet/api/src/events/BotEvent.cs
@@ -5,7 +5,18 @@
 /// </summary>
 public abstract class BotEvent : IEvent
 {
+    /// <summary>The turn number when this event occurred.</summary>
     public int TurnNumber { get; }
+
+    /// <summary>
+    /// Determines whether the event is critical.
+    /// By default, events are not critical, but subclasses can override this
+    /// to provide event-specific criticality logic.
+    /// </summary>
+    /// <returns>
+    /// <c>false</c> by default.
+    /// </returns>
     public virtual bool IsCritical => false;
+
     protected BotEvent(int turnNumber) => TurnNumber = turnNumber;
 }
