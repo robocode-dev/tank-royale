27b578d91df7f672768b40f8534746b99a7c8fb3
Yang Li <yangli2@gmail.com>
2025-05-26 20:00:23 -0700

    Inhibit type checker on import from setuptools since it is not type-annotated.

--- bot-api/python/setup.py
+++ bot-api/python/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages  # type: ignore - setuptools.setup is missing type.
 import configparser
 
 # Read version from gradle.properties file
