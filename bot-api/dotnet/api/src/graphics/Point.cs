4ecfd448d123e95aba08755b7f383551829abb78
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2025-06-08 08:48:31 +0200

    Formatting + PublicAPI

--- bot-api/dotnet/api/src/graphics/Point.cs
+++ bot-api/dotnet/api/src/graphics/Point.cs
@@ -1,91 +1,92 @@
 using System;
+using JetBrains.Annotations;
 
-namespace Robocode.TankRoyale.BotApi.Graphics
+namespace Robocode.TankRoyale.BotApi.Graphics;
+
+/// <summary>
+/// Represents an ordered pair of x and y coordinates that define a point in a two-dimensional plane.
+/// </summary>
+[PublicAPI]
+public readonly struct Point : IEquatable<Point>
 {
     /// <summary>
-    /// Represents an ordered pair of x and y coordinates that define a point in a two-dimensional plane.
+    /// Gets the x-coordinate of this Point.
     /// </summary>
-    public readonly struct Point : IEquatable<Point>
+    public readonly double X { get; }
+
+    /// <summary>
+    /// Gets the y-coordinate of this Point.
+    /// </summary>
+    public readonly double Y { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the Point structure with the specified coordinates.
+    /// </summary>
+    /// <param name="x">The x-coordinate of the point.</param>
+    /// <param name="y">The y-coordinate of the point.</param>
+    public Point(double x, double y)
     {
-        /// <summary>
-        /// Gets the x-coordinate of this Point.
-        /// </summary>
-        public readonly double X { get; }
+        X = x;
+        Y = y;
+    }
 
-        /// <summary>
-        /// Gets the y-coordinate of this Point.
-        /// </summary>
-        public readonly double Y { get; }
+    /// <summary>
+    /// Determines whether the specified object is equal to the current Point.
+    /// </summary>
+    /// <param name="obj">The object to compare with the current Point.</param>
+    /// <returns>true if the specified object is equal to the current Point; otherwise, false.</returns>
+    public override bool Equals(object obj)
+    {
+        return obj is Point point && Equals(point);
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the Point structure with the specified coordinates.
-        /// </summary>
-        /// <param name="x">The x-coordinate of the point.</param>
-        /// <param name="y">The y-coordinate of the point.</param>
-        public Point(double x, double y)
-        {
-            X = x;
-            Y = y;
-        }
+    /// <summary>
+    /// Determines whether the specified Point is equal to the current Point.
+    /// </summary>
+    /// <param name="other">The Point to compare with the current Point.</param>
+    /// <returns>true if the specified Point is equal to the current Point; otherwise, false.</returns>
+    public bool Equals(Point other)
+    {
+        return X == other.X && Y == other.Y;
+    }
 
-        /// <summary>
-        /// Determines whether the specified object is equal to the current Point.
-        /// </summary>
-        /// <param name="obj">The object to compare with the current Point.</param>
-        /// <returns>true if the specified object is equal to the current Point; otherwise, false.</returns>
-        public override bool Equals(object obj)
-        {
-            return obj is Point point && Equals(point);
-        }
+    /// <summary>
+    /// Returns the hash code for this Point.
+    /// </summary>
+    /// <returns>A hash code for the current Point.</returns>
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
 
-        /// <summary>
-        /// Determines whether the specified Point is equal to the current Point.
-        /// </summary>
-        /// <param name="other">The Point to compare with the current Point.</param>
-        /// <returns>true if the specified Point is equal to the current Point; otherwise, false.</returns>
-        public bool Equals(Point other)
-        {
-            return X == other.X && Y == other.Y;
-        }
+    /// <summary>
+    /// Returns a string that represents the current Point.
+    /// </summary>
+    /// <returns>A string that represents the current Point.</returns>
+    public override string ToString()
+    {
+        return $"{{X={X}, Y={Y}}}";
+    }
 
-        /// <summary>
-        /// Returns the hash code for this Point.
-        /// </summary>
-        /// <returns>A hash code for the current Point.</returns>
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(X, Y);
-        }
+    /// <summary>
+    /// Determines whether two Point objects are equal.
+    /// </summary>
+    /// <param name="left">The first Point to compare.</param>
+    /// <param name="right">The second Point to compare.</param>
+    /// <returns>true if the points are equal; otherwise, false.</returns>
+    public static bool operator ==(Point left, Point right)
+    {
+        return left.Equals(right);
+    }
 
-        /// <summary>
-        /// Returns a string that represents the current Point.
-        /// </summary>
-        /// <returns>A string that represents the current Point.</returns>
-        public override string ToString()
-        {
-            return $"{{X={X}, Y={Y}}}";
-        }
-
-        /// <summary>
-        /// Determines whether two Point objects are equal.
-        /// </summary>
-        /// <param name="left">The first Point to compare.</param>
-        /// <param name="right">The second Point to compare.</param>
-        /// <returns>true if the points are equal; otherwise, false.</returns>
-        public static bool operator ==(Point left, Point right)
-        {
-            return left.Equals(right);
-        }
-
-        /// <summary>
-        /// Determines whether two Point objects are not equal.
-        /// </summary>
-        /// <param name="left">The first Point to compare.</param>
-        /// <param name="right">The second Point to compare.</param>
-        /// <returns>true if the points are not equal; otherwise, false.</returns>
-        public static bool operator !=(Point left, Point right)
-        {
-            return !(left == right);
-        }
+    /// <summary>
+    /// Determines whether two Point objects are not equal.
+    /// </summary>
+    /// <param name="left">The first Point to compare.</param>
+    /// <param name="right">The second Point to compare.</param>
+    /// <returns>true if the points are not equal; otherwise, false.</returns>
+    public static bool operator !=(Point left, Point right)
+    {
+        return !(left == right);
     }
 }
--- bot-api/dotnet/api/src/graphics/SvgGraphics.cs
+++ bot-api/dotnet/api/src/graphics/SvgGraphics.cs
@@ -2,217 +2,219 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using System.Text;
+using JetBrains.Annotations;
 
-namespace Robocode.TankRoyale.BotApi.Graphics
+namespace Robocode.TankRoyale.BotApi.Graphics;
+
+/// <summary>
+/// Implementation of IGraphicsContext that generates SVG markup.
+/// </summary>
+[PublicAPI]
+public class SvgGraphics : IGraphics
 {
+    private readonly List<string> _elements = new();
+    private string _strokeColor = "#000000";
+    private string _fillColor = "none";
+    private double _strokeWidth = 1.0;
+    private string _fontFamily = "Arial";
+    private double _fontSize = 12;
+
     /// <summary>
-    /// Implementation of IGraphicsContext that generates SVG markup.
+    /// Draws a line from point (x1,y1) to point (x2,y2).
     /// </summary>
-    public class SvgGraphics : IGraphics
+    public void DrawLine(double x1, double y1, double x2, double y2)
     {
-        private readonly List<string> _elements = new();
-        private string _strokeColor = "#000000";
-        private string _fillColor = "none";
-        private double _strokeWidth = 1.0;
-        private string _fontFamily = "Arial";
-        private double _fontSize = 12;
+        _elements.Add($"<line " +
+                      $"x1=\"{Format(x1)}\" " +
+                      $"y1=\"{Format(y1)}\" " +
+                      $"x2=\"{Format(x2)}\" " +
+                      $"y2=\"{Format(y2)}\" " +
+                      $"stroke=\"{_strokeColor}\" " +
+                      $"stroke-width=\"{Format(_strokeWidth)}\" " +
+                      "/>\n");
+    }
 
-        /// <summary>
-        /// Draws a line from point (x1,y1) to point (x2,y2).
-        /// </summary>
-        public void DrawLine(double x1, double y1, double x2, double y2)
+    /// <summary>
+    /// Draws the outline of a rectangle.
+    /// </summary>
+    public void DrawRectangle(double x, double y, double width, double height)
+    {
+        _elements.Add($"<rect " +
+                      $"x=\"{Format(x)}\" " +
+                      $"y=\"{Format(y)}\" " +
+                      $"width=\"{Format(width)}\" " +
+                      $"height=\"{Format(height)}\" " +
+                      $"stroke=\"{_strokeColor}\" " +
+                      $"stroke-width=\"{Format(_strokeWidth)}\" " +
+                      $"fill=\"none\" " +
+                      "/>\n");
+    }
+
+    /// <summary>
+    /// Fills a rectangle with the current fill color.
+    /// </summary>
+    public void FillRectangle(double x, double y, double width, double height)
+    {
+        _elements.Add($"<rect " +
+                      $"x=\"{Format(x)}\" " +
+                      $"y=\"{Format(y)}\" " +
+                      $"width=\"{Format(width)}\" " +
+                      $"height=\"{Format(height)}\" " +
+                      $"fill=\"{_fillColor}\" " +
+                      $"stroke=\"none\" " +
+                      $"/>\n");
+    }
+
+    /// <summary>
+    /// Draws the outline of a circle.
+    /// </summary>
+    public void DrawCircle(double x, double y, double radius)
+    {
+        _elements.Add($"<circle " +
+                      $"cx=\"{Format(x)}\" " +
+                      $"cy=\"{Format(y)}\" " +
+                      $"r=\"{Format(radius)}\" " +
+                      $"stroke=\"{_strokeColor}\" " +
+                      $"stroke-width=\"{Format(_strokeWidth)}\" " +
+                      $"fill=\"none\" " +
+                      $"/>\n");
+    }
+
+    /// <summary>
+    /// Fills a circle with the current fill color.
+    /// </summary>
+    public void FillCircle(double x, double y, double radius)
+    {
+        _elements.Add($"<circle " +
+                      $"cx=\"{Format(x)}\" " +
+                      $"cy=\"{Format(y)}\" " +
+                      $"r=\"{Format(radius)}\" " +
+                      $"fill=\"{_fillColor}\" " +
+                      $"stroke=\"none\" " +
+                      $"/>\n");
+    }
+
+    /// <summary>
+    /// Draws the outline of a polygon defined by an array of points.
+    /// </summary>
+    public void DrawPolygon(Point[] points)
+    {
+        if (points == null || points.Length < 3)
+            return;
+
+        var pointsStr = new StringBuilder();
+        foreach (var point in points)
         {
-            _elements.Add($"<line " +
-                $"x1=\"{Format(x1)}\" " +
-                $"y1=\"{Format(y1)}\" " +
-                $"x2=\"{Format(x2)}\" " +
-                $"y2=\"{Format(y2)}\" " +
-                $"stroke=\"{_strokeColor}\" " +
-                $"stroke-width=\"{Format(_strokeWidth)}\" " +
-                "/>\n");
+            pointsStr.Append($"{Format(point.X)},{Format(point.Y)} ");
         }
 
-        /// <summary>
-        /// Draws the outline of a rectangle.
-        /// </summary>
-        public void DrawRectangle(double x, double y, double width, double height)
+        _elements.Add($"<polygon " +
+                      $"points=\"{pointsStr.ToString().Trim()}\" " +
+                      $"stroke=\"{_strokeColor}\" " +
+                      $"stroke-width=\"{Format(_strokeWidth)}\" " +
+                      $"fill=\"none\" " +
+                      $"/>\n");
+    }
+
+    /// <summary>
+    /// Fills a polygon defined by an array of points with the current fill color.
+    /// </summary>
+    public void FillPolygon(Point[] points)
+    {
+        if (points == null || points.Length < 3)
+            return;
+
+        var pointsStr = new StringBuilder();
+        foreach (var point in points)
         {
-            _elements.Add($"<rect " +
-                $"x=\"{Format(x)}\" " +
-                $"y=\"{Format(y)}\" " +
-                $"width=\"{Format(width)}\" " +
-                $"height=\"{Format(height)}\" " +
-                $"stroke=\"{_strokeColor}\" " +
-                $"stroke-width=\"{Format(_strokeWidth)}\" " +
-                $"fill=\"none\" " +
-                "/>\n");
+            pointsStr.Append($"{Format(point.X)},{Format(point.Y)} ");
         }
 
-        /// <summary>
-        /// Fills a rectangle with the current fill color.
-        /// </summary>
-        public void FillRectangle(double x, double y, double width, double height)
+        _elements.Add($"<polygon " +
+                      $"points=\"{pointsStr.ToString().Trim()}\" " +
+                      $"fill=\"{_fillColor}\" " +
+                      $"stroke=\"none\" " +
+                      $"/>\n");
+    }
+
+    /// <summary>
+    /// Draws text at the specified position.
+    /// </summary>
+    public void DrawText(string text, double x, double y)
+    {
+        _elements.Add($"<text " +
+                      $"x=\"{Format(x)}\" " +
+                      $"y=\"{Format(y)}\" " +
+                      $"font-family=\"{_fontFamily}\" " +
+                      $"font-size=\"{Format(_fontSize)}\" " +
+                      $"fill=\"{_strokeColor}\" " +
+                      $">{text}</text>\n");
+    }
+
+    /// <summary>
+    /// Sets the color used for drawing outlines.
+    /// </summary>
+    public void SetStrokeColor(Color color)
+    {
+        _strokeColor = ColorToHex(color);
+    }
+
+    /// <summary>
+    /// Sets the color used for filling shapes.
+    /// </summary>
+    public void SetFillColor(Color color)
+    {
+        _fillColor = ColorToHex(color);
+    }
+
+    /// <summary>
+    /// Sets the width of the stroke used for drawing outlines.
+    /// </summary>
+    public void SetStrokeWidth(double width)
+    {
+        _strokeWidth = width;
+    }
+
+    /// <summary>
+    /// Sets the font used for drawing text.
+    /// </summary>
+    public void SetFont(string fontFamily, double fontSize)
+    {
+        _fontFamily = fontFamily;
+        _fontSize = fontSize;
+    }
+
+    /// <summary>
+    /// Generates the SVG representation of all drawing operations.
+    /// </summary>
+    public string ToSvg()
+    {
+        var svg = new StringBuilder();
+        svg.AppendLine("<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 5000 5000\">");
+        foreach (var element in _elements)
         {
-            _elements.Add($"<rect " +
-                $"x=\"{Format(x)}\" " +
-                $"y=\"{Format(y)}\" " +
-                $"width=\"{Format(width)}\" " +
-                $"height=\"{Format(height)}\" " +
-                $"fill=\"{_fillColor}\" " +
-                $"stroke=\"none\" " +
-                $"/>\n");
+            svg.Append(element);
         }
 
-        /// <summary>
-        /// Draws the outline of a circle.
-        /// </summary>
-        public void DrawCircle(double x, double y, double radius)
-        {
-            _elements.Add($"<circle " +
-                $"cx=\"{Format(x)}\" " +
-                $"cy=\"{Format(y)}\" " +
-                $"r=\"{Format(radius)}\" " +
-                $"stroke=\"{_strokeColor}\" " +
-                $"stroke-width=\"{Format(_strokeWidth)}\" " +
-                $"fill=\"none\" " +
-                $"/>\n");
-        }
+        svg.AppendLine("</svg>");
+        return svg.ToString();
+    }
 
-        /// <summary>
-        /// Fills a circle with the current fill color.
-        /// </summary>
-        public void FillCircle(double x, double y, double radius)
-        {
-            _elements.Add($"<circle " +
-                $"cx=\"{Format(x)}\" " +
-                $"cy=\"{Format(y)}\" " +
-                $"r=\"{Format(radius)}\" " +
-                $"fill=\"{_fillColor}\" " +
-                $"stroke=\"none\" " +
-                $"/>\n");
-        }
+    /// <summary>
+    /// Clears all drawing operations.
+    /// </summary>
+    public void Clear()
+    {
+        _elements.Clear();
+    }
 
-        /// <summary>
-        /// Draws the outline of a polygon defined by an array of points.
-        /// </summary>
-        public void DrawPolygon(Point[] points)
-        {
-            if (points == null || points.Length < 3)
-                return;
+    private static string ColorToHex(Color color)
+    {
+        return $"#{color.R:X2}{color.G:X2}{color.B:X2}";
+    }
 
-            var pointsStr = new StringBuilder();
-            foreach (var point in points)
-            {
-                pointsStr.Append($"{Format(point.X)},{Format(point.Y)} ");
-            }
-
-            _elements.Add($"<polygon " +
-                $"points=\"{pointsStr.ToString().Trim()}\" " +
-                $"stroke=\"{_strokeColor}\" " +
-                $"stroke-width=\"{Format(_strokeWidth)}\" " +
-                $"fill=\"none\" " +
-                $"/>\n");
-        }
-
-        /// <summary>
-        /// Fills a polygon defined by an array of points with the current fill color.
-        /// </summary>
-        public void FillPolygon(Point[] points)
-        {
-            if (points == null || points.Length < 3)
-                return;
-
-            var pointsStr = new StringBuilder();
-            foreach (var point in points)
-            {
-                pointsStr.Append($"{Format(point.X)},{Format(point.Y)} ");
-            }
-
-            _elements.Add($"<polygon " +
-                $"points=\"{pointsStr.ToString().Trim()}\" " +
-                $"fill=\"{_fillColor}\" " +
-                $"stroke=\"none\" " +
-                $"/>\n");
-        }
-
-        /// <summary>
-        /// Draws text at the specified position.
-        /// </summary>
-        public void DrawText(string text, double x, double y)
-        {
-            _elements.Add($"<text " +
-                $"x=\"{Format(x)}\" " +
-                $"y=\"{Format(y)}\" " +
-                $"font-family=\"{_fontFamily}\" " +
-                $"font-size=\"{Format(_fontSize)}\" " +
-                $"fill=\"{_strokeColor}\" " +
-                $">{text}</text>\n");
-        }
-
-        /// <summary>
-        /// Sets the color used for drawing outlines.
-        /// </summary>
-        public void SetStrokeColor(Color color)
-        {
-            _strokeColor = ColorToHex(color);
-        }
-
-        /// <summary>
-        /// Sets the color used for filling shapes.
-        /// </summary>
-        public void SetFillColor(Color color)
-        {
-            _fillColor = ColorToHex(color);
-        }
-
-        /// <summary>
-        /// Sets the width of the stroke used for drawing outlines.
-        /// </summary>
-        public void SetStrokeWidth(double width)
-        {
-            _strokeWidth = width;
-        }
-
-        /// <summary>
-        /// Sets the font used for drawing text.
-        /// </summary>
-        public void SetFont(string fontFamily, double fontSize)
-        {
-            _fontFamily = fontFamily;
-            _fontSize = fontSize;
-        }
-
-        /// <summary>
-        /// Generates the SVG representation of all drawing operations.
-        /// </summary>
-        public string ToSvg()
-        {
-            var svg = new StringBuilder();
-            svg.AppendLine("<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 5000 5000\">");
-            foreach (var element in _elements)
-            {
-                svg.Append(element);
-            }
-            svg.AppendLine("</svg>");
-            return svg.ToString();
-        }
-
-        /// <summary>
-        /// Clears all drawing operations.
-        /// </summary>
-        public void Clear()
-        {
-            _elements.Clear();
-        }
-
-        private static string ColorToHex(Color color)
-        {
-            return $"#{color.R:X2}{color.G:X2}{color.B:X2}";
-        }
-
-        private static string Format(double value)
-        {
-            return value.ToString("0.###", CultureInfo.InvariantCulture);
-        }
+    private static string Format(double value)
+    {
+        return value.ToString("0.###", CultureInfo.InvariantCulture);
     }
 }
