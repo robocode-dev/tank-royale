17b00c0310e09cdc48cbf05a7c79df301fed3135
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2025-06-10 23:59:57 +0200

    Renaming component -> parentComponent

--- gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/util/MessageDialog.kt
+++ gui-app/src/main/kotlin/dev/robocode/tankroyale/gui/util/MessageDialog.kt
@@ -7,14 +7,14 @@ import javax.swing.JOptionPane.YES_OPTION
 
 object MessageDialog {
 
-    fun showMessage(msg: String, component: Component? = null) {
-        JOptionPane.showMessageDialog(component, msg, UiTitles.get("message"), JOptionPane.INFORMATION_MESSAGE)
+    fun showMessage(msg: String, parentComponent: Component? = null) {
+        JOptionPane.showMessageDialog(parentComponent, msg, UiTitles.get("message"), JOptionPane.INFORMATION_MESSAGE)
     }
 
-    fun showError(msg: String, component: Component? = null) {
-        JOptionPane.showMessageDialog(component, msg, UiTitles.get("error"), JOptionPane.ERROR_MESSAGE)
+    fun showError(msg: String, parentComponent: Component? = null) {
+        JOptionPane.showMessageDialog(parentComponent, msg, UiTitles.get("error"), JOptionPane.ERROR_MESSAGE)
     }
 
-    fun showConfirm(msg: String, component: Component? = null): Boolean =
-        JOptionPane.showConfirmDialog(component, msg, UiTitles.get("confirm"), JOptionPane.YES_NO_CANCEL_OPTION) == YES_OPTION
+    fun showConfirm(msg: String, parentComponent: Component? = null): Boolean =
+        JOptionPane.showConfirmDialog(parentComponent, msg, UiTitles.get("confirm"), JOptionPane.YES_NO_CANCEL_OPTION) == YES_OPTION
 }
