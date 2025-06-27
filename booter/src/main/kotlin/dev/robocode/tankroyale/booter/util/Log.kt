a1a33cd9e905a6713604851b192e096f3c907233
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2025-06-27 17:17:47 +0200

    Bugfix: The `BaseBot()` constructor (instead of `Bot()` constructor

--- booter/src/main/kotlin/dev/robocode/tankroyale/booter/util/Log.kt
+++ booter/src/main/kotlin/dev/robocode/tankroyale/booter/util/Log.kt
@@ -2,22 +2,27 @@ package dev.robocode.tankroyale.booter.util
 
 import java.nio.file.Path
 import kotlin.io.path.absolutePathString
+import kotlin.text.replace
 
 object Log {
 
     private fun printBotDir(botDir: Path?) {
-        val botDirPath = botDir?.absolutePathString()?.replace("\\", "/")
-        System.err.println("ERROR: Bot directory: ${botDirPath ?: "unknown"}")
+        val botDirPath = botDir?.absolutePathString()?.replace("\\", "\\")
+        error("ERROR: Bot directory: ${botDirPath ?: "unknown"}")
     }
 
     fun error(ex: Exception, botDir: Path? = null) {
         printBotDir(botDir)
-        System.err.println(ex.message ?: "Unknown error")
-        ex.stackTrace?.firstOrNull()?.let { System.err.println(it.toString()) }
+        error(ex.message ?: "Unknown error")
+        ex.stackTrace?.firstOrNull()?.let { println(it.toString()) }
     }
 
     fun error(message: String, botDir: Path? = null) {
         printBotDir(botDir)
-        System.err.println(message)
+        error(message)
+    }
+
+    private fun error(message: String) {
+        System.err.println(message.replace("\\", "/"))
     }
 }
