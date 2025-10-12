6669477fa349e85ae1dfcec2245c2583f9f5c202
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2025-10-12 17:59:55 +0200

    Added README with Java Properties File Naming Conventions for languages and countries etc.

--- gui/src/main/kotlin/dev/robocode/tankroyale/gui/ui/config/GuiConfigDialog.kt
+++ gui/src/main/kotlin/dev/robocode/tankroyale/gui/ui/config/GuiConfigDialog.kt
@@ -38,6 +38,8 @@ object GuiConfigPanel : JPanel(MigLayout("fill, insets 10", "[][grow]", "")) {
         LanguageOption("en", Strings.get("language.english")),
         LanguageOption("es", Strings.get("language.spanish")),
         LanguageOption("da", Strings.get("language.danish")),
+        LanguageOption("ca", Strings.get("language.catalan")),
+        LanguageOption("ca-ES-valencia", Strings.get("language.valencian")),
     )
     private val languageCombo = JComboBox(languageOptions)
 
