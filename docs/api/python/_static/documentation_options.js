c1dbd53a36674e6a45a8e28526ee62c2bec54d50
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2026-05-03 17:08:47 +0200

    chore(docs): upload documentation for v1.0.0

--- docs/api/python/_static/documentation_options.js
+++ docs/api/python/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.42.0',
+    VERSION: '1.0.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
---
+++ docs/assets/chunks/robocode-battle-anim.DJiKxISe.js
@@ -0,0 +1,1 @@
+const o="/assets/robocode-battle-anim.COdWuZMF.webp";export{o as _};
